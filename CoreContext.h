6ccc4f84df3bfb9ff7caac5a98680eb5faafb5f0
codemercenary <jsanchez@leapmotion.com>
2013-04-22 02:25:12 -0700

    Added a dependent context test, fixed a few bugs

--- CoreContext.h
+++ CoreContext.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Autowirer.h"
 #include "CoreThread.h"
+#include "CurrentContextPusher.h"
 #include "DependentContext.h"
 #include <boost/thread/condition.hpp>
 #include <boost/thread/mutex.hpp>
@@ -50,6 +51,42 @@ public:
   /// </summary>
   /// <param name="pParent">An optional parent context.  If null, will default to the root context.</param>
   cpp11::shared_ptr<CoreContext> Create(void);
+
+  /// <summary>
+  /// Creates a dependent context using the specified global structure as a reference entity
+  /// </summary>
+  /// <returns>A new dependent context with the requested members</returns>
+  /// <remarks>
+  /// The structure passed as type T is usually a simple structure containing nothing except
+  /// AutoRequired members which will populate the context with some required basic members.
+  /// The structure generally looks like this:
+  ///
+  /// struct UseCaseName {
+  ///   AutoRequired<GeneratorClass> m_generator;
+  ///   AutoRequired<FinderClass> m_finder;
+  /// };
+  ///
+  /// The dependent context has an autowired CoreContext member in it; this member is needed
+  /// in order to provide the sole guaranteed strong reference to the context that prevents
+  /// the whole thing from being freed.
+  ///
+  /// The current context is altered during this method call, but will be reset before it
+  /// returns.  The InitiateCoreThreads routine is not invoked on the created Context; the
+  /// caller is responsible for starting core threads if this is desired.
+  /// </remarks>
+  template<class T>
+  DependentContext<T>* CreateDependentContext(void) {
+    // Complex statement, here's what's going on:
+    //  1) Create a new context, child of this one, and make it current
+    //  2) Create a new dependent context
+    //  3) T's ctor initializes all of T's AutoRequired members
+    //  4) AutoRequired members insert new members into the new CoreContext
+    //  5) DependentContext ctor is called, autowires in the current context into m_context
+    //  6) pusher's dtor is called, resets the current context
+    cpp11::shared_ptr<CoreContext> dependent = Create();
+    CurrentContextPusher pusher(dependent.get());
+    return new DependentContext<T>;
+  }
 
 private:
   // General purpose lock for this class
@@ -184,50 +221,12 @@ public:
   /// </summary>
   /// <returns>The previously current context</returns>
   cpp11::shared_ptr<CoreContext> SetCurrent(void) {
-    return SetCurrent(
+    cpp11::shared_ptr<CoreContext> newCurrent = 
       cpp11::static_pointer_cast<CoreContext, Autowirer>(
         m_self.lock()
-      )
-    );
-  }
-
-  /// <summary>
-  /// Creates a dependent context using the specified global structure as a reference entity
-  /// </summary>
-  /// <returns>A new dependent context with the requested members</returns>
-  /// <remarks>
-  /// The structure passed as type T is usually a simple structure containing nothing except
-  /// AutoRequired members which will populate the context with some required basic members.
-  /// The structure generally looks like this:
-  ///
-  /// struct UseCaseName {
-  ///   AutoRequired<GeneratorClass> m_generator;
-  ///   AutoRequired<FinderClass> m_finder;
-  /// };
-  ///
-  /// The dependent context has an autowired CoreContext member in it; this member is needed
-  /// in order to provide the sole guaranteed strong reference to the context that prevents
-  /// the whole thing from being freed.
-  ///
-  /// The current context is altered during this method call, but will be reset before it
-  /// returns.  The InitiateCoreThreads routine is not invoked on the created Context; the
-  /// caller is responsible for starting core threads if this is desired.
-  /// </remarks>
-  template<class T>
-  DependentContext<T>* CreateDependentContext(void) {
-    cpp11::shared_ptr<CoreContext> self = m_self.lock();
-    if(!self)
-      throw std::exception;
-
-    // Complex statement, here's what's going on:
-    //  1) Create a new context, child of this one, and make it current
-    //  2) Create a new dependent context
-    //  3) T's ctor initializes all of T's AutoRequired members
-    //  4) AutoRequired members insert new members into the new CoreContext
-    //  5) DependentContext ctor is called, autowires in the current context into m_context
-    //  6) pusher's dtor is called, resets the current context
-    CurrentContextPusher pusher(new CoreContext(self));
-    return new DependentContext<T>;
+      );
+    ASSERT(newCurrent);
+    return SetCurrent(newCurrent);
   }
 
   /// <summary>
