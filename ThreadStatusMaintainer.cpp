e2cd06e725a116dcdf698bc04185eb79dc5601e1
codemercenary <jsanchez@leapmotion.com>
2013-05-02 13:48:41 -0700

    Added the ability to set the current thread's name, for debug purposes

--- ThreadStatusMaintainer.cpp
+++ ThreadStatusMaintainer.cpp
@@ -15,15 +15,5 @@ ThreadStatusMaintainer::~ThreadStatusMaintainer(void) {
 }
 
 void ThreadStatusMaintainer::operator()() {
-  // Make our own session current before we do anything else:
-  CurrentContextPusher pusher(pThread->GetContext());
-
-  // Now we wait for the thread to be good to go:
-  pThread->DelayUntilReady();
-  pThread->Run();
-
-  // Notify everyone that we're completed:
-  boost::lock_guard<boost::mutex>(pThread->m_lock),
-  pThread->m_completed = true,
-  pThread->m_completionCondition.notify_all();
+  pThread->DoRun();
 }
