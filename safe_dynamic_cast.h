239150e8073e475bdc523111d69b3e920a24235a
Walter Gray <wgray@leapmotion.com>
2013-05-02 18:04:55 -0700

    Fixed a compiler error on windows

--- safe_dynamic_cast.h
+++ safe_dynamic_cast.h
@@ -1,7 +1,9 @@
 #ifndef _CAST_UTILITY_H
 #define _CAST_UTILITY_H
 
+#ifdef LEAP_OS_MAC
 #include<tr1/type_traits>
+#endif //LEAP_OS_MAC
 
 template<class To, class From, bool isPolymorphic = std::tr1::is_polymorphic<From>::value>
 struct safe_dynamic_cast
