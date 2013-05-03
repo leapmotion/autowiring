13370621d40b4342305f02651a5a4d64a7bd49aa
Ted Nitz <tednitz@gmail.com>
2013-05-03 11:03:45 -0700

    This is a better fix for that problem.

--- safe_dynamic_cast.h
+++ safe_dynamic_cast.h
@@ -1,13 +1,7 @@
 #ifndef _CAST_UTILITY_H
 #define _CAST_UTILITY_H
 
-#include "ocuConfig.h"
-
-#ifdef LEAP_OS_MAC
-#include<tr1/type_traits>
-#endif //LEAP_OS_MAC
-
-template<class To, class From, bool isPolymorphic = std::tr1::is_polymorphic<From>::value>
+template<class To, class From, bool isPolymorphic = std::is_polymorphic<From>::value>
 struct safe_dynamic_cast
 {
   static To* Cast(From*) {
