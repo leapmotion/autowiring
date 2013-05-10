310fca49217c0d50419673e6e77125d417e2dadd
Kevin Horowitz <khorowitz@leapmotion.com>
2013-05-09 18:15:34 -0700

    Fixed rebase conflict.

--- MultiInheritTest.cpp
+++ MultiInheritTest.cpp
@@ -4,11 +4,38 @@
 #include "CoreContext.h"
 #include "TestFixtures/MultiInherit.h"
 
+class Shared {
+public:
+  Shared(void) {
+    m_i = 100;
+  }
+
+  int m_i;
+};
+
+class Base {
+public:
+  AutoRequired<Shared> m_member;
+};
+
+class Derived:
+  public Base
+{
+public:
+  Derived(void) {
+    EXPECT_TRUE(Base::m_member.IsAutowired()) << "Base AutoRequired member was not initialized properly";
+    EXPECT_EQ(100, Base::m_member->m_i) << "Autowired instance was not properly constructed";
+    EXPECT_EQ(m_member, m_secondMember) << "Autowiring idempotency was violated";
+  }
+
+  Autowired<Shared> m_secondMember;
+};
+
 TEST_F(MultiInheritTest, VerifyCast) {
   // Create a dummy context and make it current:
   Autowired<CoreContext> ctxt;
   ctxt.Create();
-  ctxt->SetCurrent();
+  CurrentContextPusher pshr(ctxt);
 
   // Make sure that we still have a context:
   ASSERT_TRUE(ctxt.IsAutowired()) << "Create call must create a context";
@@ -23,4 +50,15 @@ TEST_F(MultiInheritTest, VerifyCast) {
 
   // Verify that we get a pObj back with correct casting:
   EXPECT_EQ(obj, wiredPobj) << "Autowiring failed on a multiple inheritance object";
+}
+
+TEST_F(MultiInheritTest, VerifyBaseInitializer) {
+  Autowired<CoreContext> ctxt;
+  ctxt.Create();
+  CurrentContextPusher pshr(ctxt);
+
+  Derived derived;
+
+  // Expect that something autowires when we're done at least:
+  EXPECT_TRUE(derived.m_member.IsAutowired());
 }
