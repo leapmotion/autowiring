eca75b814ac1e0a8948ef6ab073bc4ee97b311e4
Jason Lokerson <cmercenary@gmail.com>
2013-05-17 16:46:55 -0700

    Code simplification, switching to an array of pointers to minimize copying

--- DispatchQueue.cpp
+++ DispatchQueue.cpp
@@ -14,35 +14,37 @@ DispatchQueue::~DispatchQueue(void) {
 void DispatchQueue::Abort(void) {
   boost::lock_guard<boost::mutex> lk(m_dispatchLock);
   m_aborted = true;
+
+  // Clear everything
   m_dispatchQueue.clear();
+
+  // Wake up anyone who is still waiting:
   m_queueUpdated.notify_all();
 }
 
-void DispatchQueue::WaitForEvent(void) {
-  std::function<void ()> fn;
-  {
-    boost::unique_lock<boost::mutex> lk(m_dispatchLock);
-    if(m_aborted)
-      throw dispatch_aborted_exception();
-
-    m_queueUpdated.wait(lk, [this] () -> bool {
-      if(m_aborted)
-        throw dispatch_aborted_exception();
-      return !this->m_dispatchQueue.empty();
-    });
-    fn = m_dispatchQueue.front();
-    m_dispatchQueue.pop_front();
-  }
-  fn();
+void DispatchQueue::DispatchEventUnsafe(boost::unique_lock<boost::mutex>& lk) {
+  std::unique_ptr<t_thunk> thunk = std::move(m_dispatchQueue.front());
+  m_dispatchQueue.pop_front();
+  lk.unlock();
+  (*thunk)();
 }
 
-void DispatchQueue::DispatchEvent(void) {
-  boost::lock_guard<boost::mutex> lk(m_dispatchLock);
+void DispatchQueue::WaitForEvent(void) {
+  boost::unique_lock<boost::mutex> lk(m_dispatchLock);
   if(m_aborted)
     throw dispatch_aborted_exception();
 
-  if(!m_dispatchQueue.empty()) {
-    m_dispatchQueue.front()();
-    m_dispatchQueue.pop_front();
-  }
+  m_queueUpdated.wait(lk, [this] () -> bool {
+    if(m_aborted)
+      throw dispatch_aborted_exception();
+    return !this->m_dispatchQueue.empty();
+  });
+  DispatchEventUnsafe(lk);
+}
+
+void DispatchQueue::DispatchEvent(void) {
+  boost::unique_lock<boost::mutex> lk(m_dispatchLock);
+  if(m_aborted)
+    throw dispatch_aborted_exception();
+  DispatchEventUnsafe(lk);
 }
