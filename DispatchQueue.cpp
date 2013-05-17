f9ef236ba69bb42f0215ce3fa249d755858bc078
codemercenary <jsanchez@leapmotion.com>
2013-05-16 17:54:24 -0700

    Adding a dispatch queue, preparing to support cross-context events

---
+++ DispatchQueue.cpp
@@ -0,0 +1,11 @@
+#include "DispatchQueue.h"
+
+DispatchQueue::DispatchQueue(void):
+  m_aborted(false)
+{
+}
+
+DispatchQueue::~DispatchQueue(void) {
+  if(!m_dispatchQueue.empty())
+    throw std::runtime_error("Dispatch queue was not empty at destructor time");
+}
---
+++ DispatchQueue.h
@@ -0,0 +1,75 @@
+#ifndef _DISPATCH_QUEUE_H
+#define _DISPATCH_QUEUE_H
+#include <boost/thread.hpp>
+#include <functional>
+#include <list>
+
+/// <summary>
+/// Thrown when a dispatch operation was aborted
+/// </summary>
+class dispatch_aborted_exception:
+  public std::exception
+{
+};
+
+/// <summary>
+/// This is an asynchronous queue of zero-argument functions
+/// </summary>
+class DispatchQueue
+{
+public:
+  DispatchQueue(void);
+
+  /// <summary>
+  /// Throws an exception if it is invoked, and Abort wasn't first called
+  /// </summary>
+  /// <remarks>
+  /// 
+  /// </remarks>
+  ~DispatchQueue(void);
+
+private:
+  bool m_aborted;
+  boost::mutex m_dispatchLock;
+  boost::condition_variable m_queueUpdated;
+  std::list<std::function<void ()>> m_dispatchQueue;
+
+public:
+  /// <summary>
+  /// Causes all currently blocked thread members to quit and prevents any blocking
+  /// </summary>
+  void Abort(void) {
+    boost::lock_guard<boost::mutex> lk(m_dispatchLock);
+    m_dispatchQueue.clear();
+  };
+
+  /// <summary>
+  /// Blocks until a new dispatch member is added, dispatches, and then returns
+  /// </summary>
+  void Wait(void) {
+    boost::unique_lock<boost::mutex> lk(m_dispatchLock);
+    if(m_aborted)
+      throw dispatch_aborted_exception();
+
+    m_queueUpdated.wait(lk, [this] () {
+      return !this->m_dispatchQueue.empty() && !m_aborted;
+    });
+    m_dispatchQueue.front()();
+    m_dispatchQueue.pop_front();
+  }
+
+  /// <summary>
+  /// Similar to Wait, but does not block
+  /// </summary>
+  void Dispatch(void) {
+    boost::lock_guard<boost::mutex> lk(m_dispatchLock);
+    if(m_aborted)
+      throw dispatch_aborted_exception();
+    if(!m_dispatchQueue.empty()) {
+      m_dispatchQueue.front()();
+      m_dispatchQueue.pop_front();
+    }
+  }
+};
+
+#endif
