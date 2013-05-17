58d44d1602da88ec396923b378e31d5fd101b37a
Jason Lokerson <cmercenary@gmail.com>
2013-05-16 20:06:54 -0700

    Plugging the mode change listener in at the correct sites

---
+++ EventReceiver.h
@@ -0,0 +1,13 @@
+#ifndef _EVENT_RECEIVER_H
+#define _EVENT_RECEIVER_H
+
+/// <summary>
+/// Used to identify event recipients
+/// </summary>
+class EventReceiver {
+public:
+  virtual ~EventReceiver(void) {
+  }
+};
+
+#endif
