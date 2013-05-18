114c9c4a95341676c3b1928726de63896dfabf5c
Jason Lokerson <cmercenary@gmail.com>
2013-05-18 01:31:31 -0700

    Eliminated unneeded hash_set dependency

--- SharedPtrHash.h
+++ SharedPtrHash.h
@@ -7,15 +7,19 @@
 /// A simple hash class that allows the use of a shared pointer in a hash_set
 /// </summary>
 template<class T>
-class SharedPtrHash:
-  public std::hash_compare<std::shared_ptr<T>>
+class SharedPtrHash
 {
 public:
+  const size_t bucket_size = 1;
+  const size_t min_buckets = 8;
+
   size_t operator()(const std::shared_ptr<T>& _Key) const {
     return (size_t)_Key.get();
   }
 
-  using std::hash_compare<std::shared_ptr<T>>::operator();
+  bool operator()(const std::shared_ptr<T>& lh, const std::shared_ptr<T>& rh) const {
+    return lh == rh;
+  }
 };
 
 #endif
