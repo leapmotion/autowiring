7e355cd65f4a5ec2bd5bdf5f8c6f2c6d91ca88c0
Jason Lokerson <cmercenary@gmail.com>
2013-05-20 04:08:46 -0700

    Partial map construction now functions as expected
    Still need to work out a pointer eviction strategy once allocation is complete

--- ContextMember.cpp
+++ ContextMember.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "ContextMember.h"
+#include "Autowired.h"
 #include "CoreContext.h"
+#include "AutoNetworkMonitor.h"
 
 ContextMember::ContextMember(const char* name):
   m_name(name),
@@ -9,4 +11,16 @@ ContextMember::ContextMember(const char* name):
 }
 
 ContextMember::~ContextMember() {
+}
+
+void* ContextMember::operator new(size_t nBytes)
+{
+  void* pRetVal = ::operator new(nBytes);
+  if(ENABLE_NETWORK_MONITOR) {
+    AutoCurrentContext ctxt;
+    shared_ptr<AutoNetworkMonitor> netMon = ctxt->FindByType<AutoNetworkMonitor>();
+    if(netMon)
+      netMon->PreConstruct(pRetVal, nBytes);
+  }
+  return pRetVal;
 }
