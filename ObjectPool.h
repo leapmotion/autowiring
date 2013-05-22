d19cdacd5e508e71aef846dcbef1b619d076ac37
Jason Lokerson <cmercenary@gmail.com>
2013-05-21 19:37:41 -0700

    Adding an object pool, intended to be used with frames

---
+++ ObjectPool.h
@@ -0,0 +1,72 @@
+#ifndef _OBJECT_POOL_H
+#define _OBJECT_POOL_H
+#include <boost/thread/mutex.hpp>
+#include <memory>
+#include <set>
+
+/// <summary>
+/// Allows the management of a pool of objects based on an embedded factory
+/// </summary>
+/// <remarks>
+/// This class is a type of factory that creates an object of type T.  The object pool
+/// creates a shared pointer for the consumer to use, and when the last shared pointer
+/// for that object is destroyed, the wrapped object is returned to this pool rather than
+/// being deleted.  Returned objects may satisfy subsequent requests for construction.
+///
+/// All object pool methods are thread safe.
+/// </remarks>
+template<class T>
+class ObjectPool
+{
+public:
+  ~ObjectPool(void) {
+    for(auto q = m_objs.begin(); q != m_objs.end(); q++)
+      delete *q;
+  }
+
+private:
+  boost::mutex m_lock;
+  typedef std::set<T*> t_stType;
+  t_stType m_objs;
+
+protected:
+  void Return(T* ptr) {
+    boost::lock_guard<boost::mutex> lk(m_lock);
+    ASSERT(m_objs.find(ptr) == m_objs.end());
+    m_objs.insert(ptr);
+  }
+
+public:
+  /// <summary>
+  /// Creates a new instance of type T and places it in the passed shared pointer
+  /// </summary>
+  void operator()(std::shared_ptr<T>& rs) {
+    T* pObj = nullptr;
+
+    {
+      boost::lock_guard<boost::mutex> lk(m_lock);
+      if(m_objs.size()) {
+        // Lock and remove an element at random:
+        t_stType::iterator q = m_objs.begin();
+        pObj = *q;
+        m_objs.erase(q);
+      }
+    }
+
+    if(!pObj)
+      // We failed to recover an object, create a new one:
+      pObj = new T;
+
+    // Fill the shared pointer with the object we created, and ensure that we override
+    // the destructor so that the object is returned to the pool when it falls out of
+    // scope.
+    rs.reset(
+      pObj,
+      [this] (T* ptr) {
+        this->Return(ptr);
+      }
+    );
+  }
+};
+
+#endif
