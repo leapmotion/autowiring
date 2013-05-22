c16975c7467576ecc3886b05c4a8eb1fc7096c37
Jason Lokerson <cmercenary@gmail.com>
2013-05-21 18:36:02 -0700

    Adding test logic for the Factory type

---
+++ FactoryTest.cpp
@@ -0,0 +1,60 @@
+#include "stdafx.h"
+#include "FactoryTest.h"
+#include "Factory.h"
+
+class SimpleInterface {
+public:
+  virtual void Method(void) = 0;
+};
+
+class ConcreteInstance:
+  public SimpleInterface
+{
+public:
+  ConcreteInstance(void):
+    m_called(false)
+  {}
+
+  bool m_called;
+
+  void Method(void) override {
+    m_called = true;
+  }
+};
+
+class SimpleFactory:
+  public IFactory<SimpleInterface>
+{
+public:
+  SimpleFactory(void):
+    m_called(false)
+  {}
+
+  bool m_called;
+
+  void operator()(SimpleInterface*& ptr) override {
+    ptr = new ConcreteInstance;
+  }
+};
+
+TEST_F(FactoryTest, VerifyFactoryWiring) {
+  // First insert our factory
+  AutoRequired<SimpleFactory> factory;
+
+  // Now we determine whether the factory gets invoked by trying to require an interface
+  AutoRequired<SimpleInterface> instance;
+
+  // Validation that the factory was called:
+  EXPECT_TRUE(factory->m_called) << "The factory wasn't called when the interface instance was autowired";
+
+  // Validation on the member:
+  ASSERT_TRUE(instance.IsAutowired()) << "Factory didn't inject a SimpleInterface instance";
+
+  // Validation on the type of the member
+  ConcreteInstance* pInstance = dynamic_cast<ConcreteInstance*>(instance.get());
+  ASSERT_TRUE(pInstance != nullptr);
+
+  // Invoke the instance member and see what happens:
+  instance->Method();
+  EXPECT_TRUE(pInstance->m_called) << "Virtual call was not correctly honored by concrete instance";
+}
