f290dd3a36795ec72bdc0cd5e0f7864cd9ccc66d
Jonathan Marsden <jmarsden@leapmotion.com>
2013-05-24 12:10:36 -0700

    Prevent the static global context shared_ptr from being initialized twice

--- GlobalCoreContext.cpp
+++ GlobalCoreContext.cpp
@@ -1,19 +1,15 @@
 #include "stdafx.h"
 #include "GlobalCoreContext.h"
 
-// We use a shared pointer, we never want the global context to go away once
-// we've created it.  We also have an initializer lock to prevent multi-init
-std::shared_ptr<GlobalCoreContext> GlobalCoreContext::s_globalContext;
-
 GlobalCoreContext::GlobalCoreContext(void):
   CoreContext(std::shared_ptr<CoreContext>())
 {
   // Guard against multi-initialization:
-  ASSERT(!s_globalContext);
+  ASSERT(!getGlobalContextSharedPtr());
 
   // Set up the global shared pointer:
-  s_globalContext.reset(this);
-  CoreContext::m_self = s_globalContext;
+  getGlobalContextSharedPtr().reset(this);
+  CoreContext::m_self = getGlobalContextSharedPtr();
 
   // Make ourselves the current context before filling it:
   SetCurrent();
@@ -24,25 +20,25 @@ GlobalCoreContext::~GlobalCoreContext(void) {
 
 // Obtains the currently generated global context:
 std::shared_ptr<GlobalCoreContext> GlobalCoreContext::Get() {
-  if(s_globalContext)
-    return s_globalContext;
-  
+  if(getGlobalContextSharedPtr())
+    return getGlobalContextSharedPtr();
+
   boost::lock_guard<boost::mutex> lk(getInitLock());
-  if(s_globalContext)
+  if(getGlobalContextSharedPtr())
     // Multi-init by another thread, just short-circuit here
-    return s_globalContext;
+    return getGlobalContextSharedPtr();
 
   // Create a new core context.  Constructor will automatically set the shared pointer.
   GlobalCoreContext* pPtr = new GlobalCoreContext();
-  ASSERT(s_globalContext);
-  ASSERT(s_globalContext.get() == pPtr);
+  ASSERT(getGlobalContextSharedPtr());
+  ASSERT(getGlobalContextSharedPtr().get() == pPtr);
 
   // Unreferenced, force a ref here to avoid complaints.
   (void)pPtr;
 
-  // We return a copy of s_globalContext to prevent the principal from being modified
+  // We return a copy of getGlobalContextSharedPtr() to prevent the principal from being modified
   // due to rvalue optimization.
-  std::shared_ptr<GlobalCoreContext> ptr(s_globalContext);
+  std::shared_ptr<GlobalCoreContext> ptr(getGlobalContextSharedPtr());
   return ptr;
 }
 
