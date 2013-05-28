034ccc82e159a8ab5309536a3884400e95953c80
codemercenary <jsanchez@leapmotion.com>
2013-05-28 10:03:32 -0700

    Switching the Pipeline class to be fully event-driven

--- ObjectPool.h
+++ ObjectPool.h
@@ -19,6 +19,11 @@ template<class T>
 class ObjectPool
 {
 public:
+  ObjectPool(size_t limit = -1):
+    m_limit(limit),
+    m_outstanding(0)
+  {}
+
   ~ObjectPool(void) {
     for(auto q = m_objs.begin(); q != m_objs.end(); q++)
       delete *q;
@@ -29,17 +34,34 @@ private:
   typedef std::set<T*> t_stType;
   t_stType m_objs;
 
+  size_t m_limit;
+  size_t m_outstanding;
+
 protected:
   void Return(T* ptr) {
     boost::lock_guard<boost::mutex> lk(m_lock);
-    ASSERT(m_objs.find(ptr) == m_objs.end());
     m_objs.insert(ptr);
+    m_outstanding--;
   }
 
 public:
   /// <summary>
+  /// Sets the maximum number of objects this pool will permit to be outstanding at time
+  /// </summary>
+  /// <remarks>
+  /// </remarks>
+  void SetOutstandingLimit(size_t limit) {
+    m_limit = limit;
+  }
+
+  /// <summary>
   /// Creates a new instance of type T and places it in the passed shared pointer
   /// </summary>
+  /// <remarks>
+  /// If the outstanding count is set to anything except -1, this method could potentially
+  /// fail and the passed shared_ptr will have a null value.  Callers who use a nonnegative
+  /// outstanding limit should be careful to check the return of this function.
+  /// </remarks>
   void operator()(std::shared_ptr<T>& rs) {
     T* pObj = nullptr;
 
@@ -53,9 +75,17 @@ public:
       }
     }
 
-    if(!pObj)
+    if(!pObj) {
+      if(m_limit == m_outstanding)
+        // We do not allow an outstanding count
+        return;
+
+      // Increment the total number of objects outstanding:
+      m_outstanding++;
+
       // We failed to recover an object, create a new one:
       pObj = new T;
+    }
 
     // Fill the shared pointer with the object we created, and ensure that we override
     // the destructor so that the object is returned to the pool when it falls out of
