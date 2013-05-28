dbb1dedc2fc3ff70515b29255b227edbc4df7a91
Hua Yang <hyang@leapmotion.com>
2013-05-28 12:33:12 -0700

    Fix a build error

--- EnclosedContextTestBase.cpp
+++ EnclosedContextTestBase.cpp
@@ -10,6 +10,7 @@ EnclosedContextTestBase::~EnclosedContextTestBase(void) {
   m_create->SignalShutdown();
 
   // Do not allow teardown to take more than a millisecond
-  if(!m_create->Wait(boost::chrono::milliseconds(100)))
+  if(!m_create->Wait(static_cast<boost::chrono::duration<double, boost::milli> >(100.))) {
     ASSERT(false);
+  }
 }
