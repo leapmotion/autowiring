b2b188aed2862bf692cdbaa28cf3200f9b8124e9
Kevin Horowitz <khorowitz@leapmotion.com>
2013-06-10 23:19:33 -0700

    Added copyright headers.

--- ExceptionFilterTest.h
+++ ExceptionFilterTest.h
@@ -1,3 +1,4 @@
+// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
 #ifndef _EXCEPTION_FILTER_TEST_H
 #define _EXCEPTION_FILTER_TEST_H
 #include "EnclosedContextTestBase.h"
