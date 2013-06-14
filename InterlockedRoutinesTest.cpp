a337af6ae2f3ef4f90b30744eeb76c866be4a028
Jason Lokerson <cmercenary@gmail.com>
2013-06-14 10:02:51 -0700

    Added a pathological test for CAS routines

--- InterlockedRoutinesTest.cpp
+++ InterlockedRoutinesTest.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "InterlockedRoutinesTest.h"
 #include "InterlockedExchange.h"
+#include <boost/thread/barrier.hpp>
+#include <boost/thread/thread.hpp>
 
 template<void* (*fn)(void*volatile*, void*)>
 void CheckFn() {
@@ -46,3 +48,48 @@ TEST_F(InterlockedRoutinesTest, VerifyCompareExchange) {
   EXPECT_EQ(&spot1, pExchanged) << "Return value under a successful exchange was not the original value";
 }
 
+TEST_F(InterlockedRoutinesTest, VerifyCompareExchangePathological) {
+  static const int threadCount = 100;
+
+  // State-checking value.  The value of this address isn't actually used, it's only here
+  // to give us a convenient address we can start from.
+  char base = 100;
+
+  // A counter, implemented as a pointer, whose value we will monotonically increase.
+  void* counter = &base;
+
+  // Barrier, in order to exacerbate race conditions:
+  boost::barrier bar(threadCount);
+
+  auto contender = [&counter, &bar] () {
+    void* presumption;
+    void* addend;
+
+    bar.wait();
+
+    do {
+      // This is an implementation of interlocked_add that is based on compare-exchange.
+      // It works by peeking the current value of the counter, computing the new value,
+      // and then swapping the newly computed value if and only if the original value is
+      // unchanged.
+      presumption = counter;
+      addend = (char*)presumption + 1;
+
+      // Transition allowable only if the addend's value hasn't changed since we computed it
+    } while(compare_exchange(&counter, addend, presumption) != presumption);
+  };
+
+  boost::thread allThreads[threadCount];
+  for(size_t i = 0; i < threadCount; i++) {
+    allThreads[i] = boost::thread(contender);
+  }
+
+  // Wait for all threads to complete:
+  for(size_t i = 0; i < threadCount; i++)
+    allThreads[i].join();
+
+  // Verify that no illegal transitions have taken place by ensuring the count is precisely
+  // the count we expect:
+  int offset = (char*)counter - &base;
+  EXPECT_EQ(offset, threadCount) << "Interlocked exchange under heavy contention failed to honor interlocking requirements";
+}
