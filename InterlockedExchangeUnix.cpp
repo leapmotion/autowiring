d2f54114dd510b3431e1627f57031c100ec6bc9f
Jonathan Marsden <jmarsden@leapmotion.com>
2013-06-14 10:46:25 -0700

    Use builtin GCC/Clang atomic exchange functions

--- InterlockedExchangeUnix.cpp
+++ InterlockedExchangeUnix.cpp
@@ -2,23 +2,13 @@
 #include "InterlockedExchange.h"
 
 void* exchange_acquire(void*volatile* dest, void* exchange) {
-  __asm__ ("xchg %0, %1" : "+q" (exchange), "+m" (*dest));
-  __asm__ __volatile__ ("" ::: "memory");
-  return exchange;
+  return __sync_lock_test_and_set((void**)dest, exchange);
 }
 
 void* exchange_release(void*volatile* dest, void* exchange) {
-  __asm__ __volatile__ ("" ::: "memory");
-  __asm__ ("xchg %0, %1" : "+q" (exchange), "+m" (*dest));
-  return exchange;
+  return __sync_lock_test_and_set((void**)dest, exchange);
 }
 
 void* compare_exchange(void*volatile* dest, void* exchange, const void* comparand) {
-  void* previous = const_cast<void*>(comparand);
-  __asm__ __volatile__ ("" ::: "memory");
-  __asm__ ("lock ; cmpxchg %2, %1" : "+a" (previous), "+m" (*dest) : "q" (exchange));
-  if (previous == comparand) {
-    __asm__ __volatile__ ("" ::: "memory");
-  }
-  return previous;
+  return __sync_val_compare_and_swap((void**)dest, (void*)comparand, exchange);
 }
