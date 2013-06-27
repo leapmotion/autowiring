4db9c4d46d34d0a371de62192ced127078ac508d
codemercenary <jsanchez@leapmotion.com>
2013-06-26 22:57:19 -0700

    Adding a unit test to validate construction success at an address where a prior autowiring failed

--- ScopeTest.cpp
+++ ScopeTest.cpp
@@ -4,16 +4,7 @@
 #include "GlobalCoreContext.h"
 #include "Autowired.h"
 
-ScopeTest::ScopeTest(void)
-{
-}
-
-ScopeTest::~ScopeTest(void)
-{
-}
-
-TEST_F(ScopeTest, VerifyGlobalExists)
-{
+TEST_F(ScopeTest, VerifyGlobalExists) {
   // Verify that we at least get a global scope
   Autowired<GlobalCoreContext> global;
   EXPECT_TRUE(global.IsAutowired()) << "Failed to autowire the global context";
