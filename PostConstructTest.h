ce947cdc0e8f124041128d6bb352f5d0d552b81e
Jason Lokerson <cmercenary@gmail.com>
2013-06-26 22:57:19 -0700

    Adding a unit test to validate construction success at an address where a prior autowiring failed

--- PostConstructTest.h
+++ PostConstructTest.h
@@ -1,10 +1,10 @@
 // Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
 #ifndef _POST_CONSTRUCT_TEST_H
 #define _POST_CONSTRUCT_TEST_H
-#include <gtest/gtest.h>
+#include "EnclosedContextTestBase.h"
 
 class PostConstructTest:
-  public testing::Test
+  public EnclosedContextTestBase
 {
 };
 
