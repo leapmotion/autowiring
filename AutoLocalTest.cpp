3c85e7600e6e7ce91d01145ef39f785bf0d4686c
Jason Lokerson <cmercenary@gmail.com>
2013-07-03 18:09:57 -0700

    Adding new AutoLocal types and corresponding tests

---
+++ AutoLocalTest.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "AutoLocalTest.h"
+#include "CoreContext.h"
+
+class StrictlyLocal {
+};
+
+TEST_F(AutoLocalTest, VerifyNegativeAutowiring) {
+  AutoRequired<StrictlyLocal> exterior;
+
+  {
+    AutoCreateContext ctxt;
+    CurrentContextPusher pshr(ctxt);
+
+    // Attempt a local autowiring on a type that doesn't exist:
+    AutowiredLocal<StrictlyLocal> local;
+    EXPECT_FALSE(local.IsAutowired()) << "Locally autowired a type which was not local to the current context";
+  }
+}
+
+TEST_F(AutoLocalTest, VerifyNoExteriorDeferred) {
+  // Create a subcontext first:
+  AutoCreateContext ctxt;
+  CurrentContextPusher pshr(ctxt);
+
+  // Attempt to autowire locally to this context--should fail:
+  AutowiredLocal<StrictlyLocal> local;
+  ASSERT_FALSE(local.IsAutowired());
+
+  // Back out, and verify that an exterior AutoRequired type doesn't cause the local slot to receive a pointer:
+  pshr.Pop();
+  AutoRequired<StrictlyLocal> created;
+  EXPECT_FALSE(local.IsAutowired()) << "A creation event unexpectedly satisfied a local pointer in a child context";
+}
