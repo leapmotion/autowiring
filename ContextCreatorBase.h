b916d16178c29646c396b589f414253086b51957
Jason Lokerson <cmercenary@gmail.com>
2013-07-03 19:58:04 -0700

    Some redundant code moved to the base type, eliminated some inapplicable methods

--- ContextCreatorBase.h
+++ ContextCreatorBase.h
@@ -21,11 +21,45 @@ protected:
   /// <summary>
   /// Utility method for clearing an arbitrary collection:
   /// </summary>
+  /// <param name="ctr">The collection of contexts to be cleared</param>
+  /// <param name="iter">An iterator locker, which can obtain a shared pointer from an iterator</param>
   /// <remarks>
   /// This method is synchronized on the contextLock and this will generally make it thread-safe
   /// </remarks>
   template<class Ctr, class Fx>
-  void Clear(Ctr& ctr) {
+  void Clear(Ctr& ctr, Fx&& locker) {
+    if(!wait) {
+      // Trivial signal-clear-return:
+      boost::lock_guard<boost::mutex> lk(m_contextLock);
+      for(auto q = ctr.begin(); q != ctr.end(); q++) {
+        auto locked = locker(q);
+        if(locked)
+          locked->SignalShutdown();
+      }
+      ctr.clear();
+      return;
+    }
+
+    Ctr ctrCopy;
+
+    // Copy out and clear:
+    {
+      boost::lock_guard<boost::mutex> lk(m_contextLock);
+      for(auto q = ctr.begin(); q != ctr.end(); q++) {
+        auto locked = locker(q);
+        if(locked)
+          locked->SignalShutdown();
+      }
+      ctrCopy = ctr;
+      ctr.clear();
+    }
+
+    // Signal everyone first, then wait in a second pass:
+    for(auto q = ctrCopy.begin(); q != ctrCopy.end(); q++) {
+      auto locked = locker(q);
+      if(locked)
+        locked->Wait();
+    }
   }
 };
 
