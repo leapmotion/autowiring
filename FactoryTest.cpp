be5bb1939863609a928b78047f72dbadf662534a
Jason Lokerson <cmercenary@gmail.com>
2013-10-18 15:11:04 -0700

    Adding a simple ctor detector and corresponding test validations

--- FactoryTest.cpp
+++ FactoryTest.cpp
@@ -8,26 +8,46 @@ using namespace std;
 
 class SimpleInterface {
 public:
-  virtual void Method(void) = 0;
-};
-
-class ClassWithStaticAllocator:
-  public SimpleInterface
-{
-public:
-  ClassWithStaticAllocator(void):
+  SimpleInterface(void):
     m_called(false)
   {}
 
   bool m_called;
 
-  static ClassWithStaticAllocator* New(void) {return new ClassWithStaticAllocator();}
+  virtual void Method(void) = 0;
 
+  // Factory method, for trivial factory construction:
+  static SimpleInterface* New(void);
+};
+static_assert(has_static_new<SimpleInterface>::value, "Class with static allocator was not correctly detected as having one");
+
+/// <summary>
+/// Simple constructor
+/// </summary>
+class ClassWithSimpleCtor:
+  public SimpleInterface
+{
+public:
   void Method(void) override {
     m_called = true;
   }
 };
-static_assert(has_static_new<ClassWithStaticAllocator>::value, "Class with static allocator was not correctly detected as having one");
+static_assert(has_simple_constructor<ClassWithSimpleCtor>::value, "Class with a zero-argument constructor was not correctly identified as such");
+
+class ClassWithIntegralCtor:
+  public SimpleInterface
+{
+public:
+  ClassWithIntegralCtor(int)
+  {
+  }
+};
+static_assert(!has_simple_constructor<ClassWithIntegralCtor>::value, "A class without a simple constructor was incorrectly identified as having one");
+
+// Now we can define the factory method, once ClassWithSimpleCtor has been defined.
+SimpleInterface* SimpleInterface::New(void) {
+  return new ClassWithSimpleCtor();
+}
 
 TEST_F(FactoryTest, VerifyFactoryWiring) {
 
