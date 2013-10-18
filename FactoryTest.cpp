92e5e42d9e9483fbcc946f4d317c8c58637cadd1
Jason Lokerson <cmercenary@gmail.com>
2013-10-18 15:03:28 -0700

    Eliminating some obsolescent test code

--- FactoryTest.cpp
+++ FactoryTest.cpp
@@ -11,64 +11,24 @@ public:
   virtual void Method(void) = 0;
 };
 
-class ConcreteInstance:
+class ClassWithStaticAllocator:
   public SimpleInterface
 {
 public:
-  ConcreteInstance(void):
+  ClassWithStaticAllocator(void):
     m_called(false)
   {}
 
   bool m_called;
 
-  static ConcreteInstance* New(void);
+  static ClassWithStaticAllocator* New(void) {return new ClassWithStaticAllocator();}
 
   void Method(void) override {
     m_called = true;
   }
 };
-
-class SimpleFactory:
-  public AutoFactory<SimpleInterface>
-{
-public:
-  SimpleFactory(void):
-    m_called(false)
-  {}
-
-  virtual ~SimpleFactory() { }
-
-  bool m_called;
-
-  SimpleInterface* operator()(void) override {
-    m_called = true;
-    return new ConcreteInstance;
-  }
-};
-
+static_assert(has_static_new<ClassWithStaticAllocator>::value, "Class with static allocator was not correctly detected as having one");
 
 TEST_F(FactoryTest, VerifyFactoryWiring) {
-  static_assert(has_static_new<ConcreteInstance>::value, "Good is bad!");
-  static_assert(!has_static_new<SimpleFactory>::value, "Bad is good!");
 
-  // First insert our factory
-  AutoRequired<SimpleFactory> factory;
-
-  // Now we determine whether the factory gets invoked by trying to require an interface
-  // TODO:  Change this to AutoRequired and correct the resulting compiler errors
-  Autowired<SimpleInterface> instance;
-
-  // Validation that the factory was called:
-  EXPECT_TRUE(factory->m_called) << "The factory wasn't called when the interface instance was autowired";
-
-  // Validation on the member:
-  ASSERT_TRUE(instance.IsAutowired()) << "Factory didn't inject a SimpleInterface instance";
-
-  // Validation on the type of the member
-  ConcreteInstance* pInstance = dynamic_cast<ConcreteInstance*>(instance.get());
-  ASSERT_TRUE(pInstance != nullptr);
-
-  // Invoke the instance member and see what happens:
-  instance->Method();
-  EXPECT_TRUE(pInstance->m_called) << "Virtual call was not correctly honored by concrete instance";
 }
