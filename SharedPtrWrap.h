0fcac87cd56fbb077cd2102e3430a5a6ed204d10
codemercenary <jsanchez@leapmotion.com>
2013-10-23 20:46:27 -0700

    Linux compiler fixes

--- SharedPtrWrap.h
+++ SharedPtrWrap.h
@@ -16,12 +16,12 @@ namespace std {
   /// Identical to static_pointer_cast if U inherits T, dynamic_pointer_cast otherwise
   /// </summary>
   template<class T, class U>
-  inline typename std::enable_if<std::is_base_of<T, U>::value, std::shared_ptr<T>>::type fast_pointer_cast(typename const std::shared_ptr<U>& Other) {
+  typename std::enable_if<std::is_base_of<T, U>::value, std::shared_ptr<T>>::type fast_pointer_cast(const std::shared_ptr<U>& Other) {
     return std::static_pointer_cast<T, U>(Other);
   };
 
   template<class T, class U>
-  inline typename std::enable_if<
+  typename std::enable_if<
     !std::is_base_of<T, U>::value &&
     std::is_polymorphic<T>::value &&
     std::is_polymorphic<U>::value,
@@ -31,7 +31,11 @@ namespace std {
   }
 
   template<class T, class U>
-  inline std::shared_ptr<T> fast_pointer_cast(...) {
+  typename std::enable_if<
+    !std::is_polymorphic<T>::value ||
+    !std::is_polymorphic<U>::value,
+    std::shared_ptr<T>
+  >::type fast_pointer_cast(const std::shared_ptr<U>&) {
     return std::shared_ptr<T>();
   }
 }
