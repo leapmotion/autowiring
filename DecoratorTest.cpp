503b604c2ea9e86da2f3dc590e05d4ac88ed547f
Jason Lokerson <cmercenary@gmail.com>
2013-11-03 16:59:03 -0800

    Stubbing out a first-class packet factory/filter system

--- DecoratorTest.cpp
+++ DecoratorTest.cpp
@@ -1,9 +1,39 @@
 #include "stdafx.h"
 #include "DecoratorTest.h"
+#include "FilterPropertyExtractor.h"
+
+using namespace std;
+
+/// <summary>
+/// A simple "decoration" class
+/// </summary>
+template<int i>
+class Decoration {
+public:
+  Decoration(void) :
+    i(i)
+  {}
+
+  int i;
+};
 
 class DecoratorA {
 public:
+  void Filter(Decoration<0> zero, Decoration<1> one);
 };
+
+TEST_F(DecoratorTest, VerifyCorrectExtraction) {
+  vector<const type_info*> v;
+
+  // Run our prop extractor based on a known decorator:
+  RecipientPropertyExtractor<DecoratorA>::Extract(
+    [&v] (const std::type_info& ti) {
+      v.push_back(&ti);
+    }
+  );
+
+  ASSERT_EQ(2, v.size()) << "Extracted an insufficient number of types from a known filter function";
+}
 
 TEST_F(DecoratorTest, VerifySimpleRecipient) {
 
---
+++ FilterPropertyExtractor.h
@@ -0,0 +1,43 @@
+#pragma once
+
+template<typename T>
+struct has_filter
+{
+  template<class Fn, Fn>
+  struct unnamed_constant;
+
+  template<class U>
+  static int select(unnamed_constant<decltype(&U::Filter), &U::Filter>*);
+
+  template<class U>
+  static char select(...);
+
+  static const bool value = sizeof(select<T>(nullptr)) == sizeof(int);
+};
+
+/// <summary>
+/// Utility class which allows discovery of the named 
+/// </summary>
+template<class T, bool hasFilter = has_filter<T>::value>
+class RecipientPropertyExtractor {
+public:
+  /// <summary>
+  /// Passes detected filter inputs to the specified functional
+  /// </summary>
+  /// <param name="fx">A function of the form fx(const std::type_info&)</param>
+  /// <remarks>
+  /// This method can be called on any type, and will pass the arguments of the
+  /// single detected "Filter" method, one at a time, to the passed functional.
+  ///
+  /// 
+  template<class _Fx>
+  static void Extract(_Fx&& fx) {
+
+  }
+};
+
+template<class T>
+class RecipientPropertyExtractor<T, false> {
+  template<class _Fx>
+  static void Extract(_Fx&&) {}
+};
