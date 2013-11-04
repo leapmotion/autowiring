d716f2465a27e529838cd8d24943d4353ed34a7b
codemercenary <jsanchez@leapmotion.com>
2013-11-04 01:53:26 -0800

    Miscellaneous bug fixes with the way function types are represented to callers

--- AutoPacket.cpp
+++ AutoPacket.cpp
@@ -3,12 +3,6 @@
 #include "AutoPacketFactory.h"
 #include <boost/any.hpp>
 
-AutoPacketSubscriberStatus::AutoPacketSubscriberStatus(const AutoPacketSubscriber& subscriber) :
-  m_pSubscriber(&subscriber),
-  m_visitCount(subscriber.GetDegree())
-{
-}
-
 AutoPacket::AutoPacket()
 {
 }
@@ -18,18 +12,21 @@ AutoPacket::~AutoPacket()
 }
 
 void AutoPacket::UpdateSatisfaction(const std::type_info& info) {
-  size_t subscriberIndex = m_factory->FindSubscriberIndex(info);
-  if(subscriberIndex == -1)
+  auto decorator = m_factory->FindDecorator(info);
+  if(!decorator)
     return;
 
-  // Obtain the corresponding index:
-  if(!m_satCounters[subscriberIndex]--)
-    __noop();
+  // Update all satisfaction counters:
+  const auto& satVec = m_factory->GetSubscriberVector();
+  const auto& subscribers = decorator->subscribers;
+  for(auto q = subscribers.begin(); q != subscribers.end(); q++)
+    if(!--m_satCounters[*q])
+      satVec[*q].Call(*this);
 }
 
 void AutoPacket::Reset(void) {
   auto& vec = m_factory->GetSubscriberVector();
   m_satCounters.resize(vec.size());
   for(size_t i = vec.size(); i--; )
-    m_satCounters[i] = vec[i];
+    m_satCounters[i] = vec[i].GetArity();
 }
