aa159a7fa21c75f91927c6bd5bdb48910c28c4f4
Jason Lokerson <cmercenary@gmail.com>
2013-11-05 21:39:46 -0800

    Adding a test to ensure proper optional invocation in the event of an omitted decoration

--- OptionalDecorationTest.cpp
+++ OptionalDecorationTest.cpp
@@ -6,12 +6,16 @@
 class OptionalFilter {
 public:
   OptionalFilter(void) :
+    m_hadOptional(false),
     m_called(false)
   {}
 
   void AutoFilter(optional_ptr<Decoration<0>> optional, Decoration<1> required) {
+    m_called = true;
+    m_hadOptional = optional;
   }
 
+  bool m_hadOptional;
   bool m_called;
 };
 
@@ -24,5 +28,23 @@ TEST_F(OptionalDecorationTest, EnsureOrdinarySatisfaction) {
   packet->Decorate(Decoration<0>());
   packet->Decorate(Decoration<1>());
 
+  EXPECT_TRUE(filter->m_hadOptional) << "Expected, but did not receive, an optional pointer";
   EXPECT_TRUE(filter->m_called) << "Optional decoration was not correctly parsed";
+}
+
+TEST_F(OptionalDecorationTest, EnsureOmittedSatisfaction) {
+  AutoRequired<OptionalFilter> filter;
+  AutoRequired<AutoPacketFactory> factory;
+
+  {
+    // Obtain a new packet and only add the mandatory decoration:
+    auto packet = factory->NewPacket();
+    packet->Decorate(Decoration<1>());
+
+    // Adding the mandatory decoration should not have been adequate to satisfy the input:
+    EXPECT_FALSE(filter->m_called) << "Filter called before the packet was released";
+  }
+
+  EXPECT_FALSE(filter->m_hadOptional) << "Optional pointer was passed even though no optional decoration was set";
+  EXPECT_TRUE(filter->m_called) << "Optional call was not correctly made";
 }
