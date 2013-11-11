b64de7977e5876220a977c806250af90855e8cd7
Jason Lokerson <cmercenary@gmail.com>
2013-11-11 10:27:39 -0800

    Small additional assertion to make AutoRequired abstract classes easier to diagnose

--- Autowired.h
+++ Autowired.h
@@ -68,6 +68,7 @@ public:
 
   template<class U>
   static typename std::enable_if<!has_static_new<U>::value, U*>::type New(void) {
+    static_assert(!std::is_abstract<U>::value, "Cannot create a type which is abstract");
     static_assert(has_simple_constructor<U>::value, "Attempted to create a type which did not provide a zero-arguments ctor");
     return new U;
   }
