1aadbad73e3097c7121f83aedc43deb91ff7cfad
Max Sills <sillsm@gmail.com>
2013-11-11 12:47:58 -0800

    Much prettier

--- ReentrantCounter.cpp
+++ ReentrantCounter.cpp
@@ -1,24 +1,16 @@
 #include "stdafx.h"
 #include "ReentrantCounter.h"
 
-size_t ReentrantCounter::numberOfTimesEntered = 0;
-size_t ReentrantCounter::currentLevel = 0;
+ReentrantCounter * ReentrantCounter::lastKnownObject = 0;
 boost::chrono::nanoseconds ReentrantCounter::globalTimeElapsedSinceStart(0);  
 
 ReentrantCounter::ReentrantCounter(PerformanceCounter& duration) :
   duration(duration),
   startTime(boost::chrono::high_resolution_clock::now())
 {
-  iAmARoot = (currentLevel == 0) ?true:false;
-  if(iAmARoot){
-  globalTimeElapsedSinceStart = (boost::chrono::nanoseconds(0)); 
-  numberOfTimesEntered = 0;
-  }
-  currentLevel++;
-  numberOfTimesEntered++;
-  myRecordedTimeElapsedSinceStart = globalTimeElapsedSinceStart;
-  timesEnteredSinceIStarted = numberOfTimesEntered;
-  //std::cout<<numberOfTimesEntered <<"OK?"<< globalTimeElapsedSinceStart.count()<< std::endl;
+  objectIObservedOnStart = lastKnownObject;
+  lastKnownObject = this;
+  if(!objectIObservedOnStart)globalTimeElapsedSinceStart = (boost::chrono::nanoseconds(0)); 
  }
 
 ReentrantCounter::~ReentrantCounter(void) 
@@ -27,16 +19,14 @@ ReentrantCounter::~ReentrantCounter(void)
   endTime= boost::chrono::high_resolution_clock::now();
   // Unconditionally increment the hit count on this field:
   duration.hitCount++;
-  bool iAmALeaf = (timesEnteredSinceIStarted == numberOfTimesEntered) ?true:false;
-  if (iAmALeaf) {
+  if (lastKnownObject == this) {
   //std::cout<<"I am a leaf"<<std::endl;
   duration.lingerTime = endTime - startTime;
-  } else if (!iAmALeaf){
+  } else{
+  // std::cout<<"I am not a leaf"<<std::endl;
   boost::chrono::nanoseconds difference = globalTimeElapsedSinceStart - myRecordedTimeElapsedSinceStart;
   duration.lingerTime = endTime - startTime - difference;
   }
   globalTimeElapsedSinceStart +=duration.lingerTime;
-  //std::cout<<"Here is what I believe my global time elapsed since start is " << globalTimeElapsedSinceStart.count() << std::endl;
-
-  currentLevel--;
+  if (!objectIObservedOnStart) lastKnownObject = 0;
 }
