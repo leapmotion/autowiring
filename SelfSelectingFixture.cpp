6e7b7ecdaa68e52f636798ee84ef50ca5a05110a
codemercenary <jsanchez@leapmotion.com>
2013-11-18 14:45:21 -0800

    Adding a microbolt concept, fixture updated to make use of it

---
+++ SelfSelectingFixture.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "SelfSelectingFixture.h"
+#include "MicroBolt.h"
+
+const char s_expectedFixtureName[] = "SelfSelect";
+
+BOLT_TO(SelfSelectingFixture, SelfSelect);
+
+SelfSelectingFixture::SelfSelectingFixture(void) :
+  magic(10010)
+{
+}
+
+bool SelfSelectingFixture::IsMagicCorrect(void) const {
+  return magic == 10010;
+}
