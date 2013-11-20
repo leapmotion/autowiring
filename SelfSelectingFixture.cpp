bfb95a347a84ef42778a6fc9aa896cd5755d5381
codemercenary <jsanchez@leapmotion.com>
2013-11-20 09:31:32 -0800

    One-liner declaration now used by the SelfSelectingFixture

--- SelfSelectingFixture.cpp
+++ SelfSelectingFixture.cpp
@@ -3,13 +3,7 @@
 #include "MicroBolt.h"
 
 const char s_expectedFixtureName[] = "SelfSelect";
-const char s_pipelinename[] = "Global/IAMANON/SelfSelect";
-
-//BOLT_TO(SelfSelectingFixture, SelfSelect);
-//static MicroBolt<SelfSelectingFixture, s_expectedFixtureName> mymicro;
-//static const MicroBolt<SelfSelectingFixture, s_pipelinename> mymicro;
-
-static AutoRequired<MicroBolt<SelfSelectingFixture, s_pipelinename>> m_b_l_static_no_clash_doo_doo;
+BOLT_TO(SelfSelectingFixture, "Global/IAMANON/SelfSelect")
 
 SelfSelectingFixture::SelfSelectingFixture(void) :
   magic(10010)
