0dd67ea991bd5bbf5c386e33076e3648d17928ab
Jason Lokerson <cmercenary@gmail.com>
2013-11-26 16:59:39 -0800

    fixup! Adding a user-specified ability to enforce single owner teardown at destructor time

--- CoreContext.h
+++ CoreContext.h
@@ -422,7 +422,7 @@ public:
 
     // Ownership validation, as appropriate:
     if(m_useOwnershipValidator)
-      PendValidation(value);
+      PendValidation(std::weak_ptr<T>(value));
   }
 
   /// <summary>
