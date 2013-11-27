f36b98c13da585cfaa4d6c0d93baa4c0ad473766
Jason Lokerson <cmercenary@gmail.com>
2013-11-26 17:38:55 -0800

    Making unit test assumptions more explicit

--- SimpleOwnershipValidatorTest.cpp
+++ SimpleOwnershipValidatorTest.cpp
@@ -15,16 +15,19 @@ TEST_F(SimpleOwnershipValidatorTest, VerifyExclusionCases) {
 
   std::shared_ptr<SimpleObject> obj(new SimpleObject);
   std::weak_ptr<SimpleObject> objWeak(obj);
+  std::weak_ptr<CoreContext> ctxtWeak;
   {
     AutoCreateContext ctxt;
     ctxt->EnforceSimpleOwnership();
     ctxt->AddValidationListener(fn);
+    ctxtWeak = ctxt;
 
     // Add an arbitrary object to this context:
     ctxt->Add(obj);
   }
 
   // Verify we got hit, at a minimum:
+  ASSERT_TRUE(ctxtWeak.expired()) << "Context did not expire when it went out of scope as expected";
   ASSERT_TRUE(hit) << "Validation listener was not invoked, ownership check was not enforced";
   ASSERT_EQ(1UL, count) << "An insufficient number of entities were detected as being incorrectly owned";
 
@@ -36,6 +39,7 @@ TEST_F(SimpleOwnershipValidatorTest, VerifyExclusionCases) {
     AutoCreateContext ctxt;
     ctxt->EnforceSimpleOwnership();
     ctxt->AddValidationListener(fn);
+    ctxtWeak = ctxt;
 
     // Add an arbitrary object to this context, but release the pointer this time:
     ctxt->Add(obj);
@@ -43,6 +47,7 @@ TEST_F(SimpleOwnershipValidatorTest, VerifyExclusionCases) {
   }
 
   // Now verify that the object was destroyed as expected:
+  ASSERT_TRUE(ctxtWeak.expired()) << "Context did not expire when it went out of scope as expected";
   ASSERT_TRUE(objWeak.expired()) << "Weak pointer to enclosure object was not cleaned up correctly";
   ASSERT_NE(0UL, count) << "A validation listener was invoked on a list of size zero, this is an error";
   ASSERT_EQ(-1, count) << "A validation listener was invoked even though it should not have been called";
