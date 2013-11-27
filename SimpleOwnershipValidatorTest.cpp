d61df2632f9c66222d5c924710a6cb14d6a53ae3
Jason Lokerson <cmercenary@gmail.com>
2013-11-26 17:10:57 -0800

    Validation listener checks strengthened, added a test to verify countercase

--- SimpleOwnershipValidatorTest.cpp
+++ SimpleOwnershipValidatorTest.cpp
@@ -2,27 +2,47 @@
 #include "SimpleOwnershipValidatorTest.h"
 #include "TestFixtures/SimpleObject.h"
 
-TEST_F(SimpleOwnershipValidatorTest, VerifySimpleExclusion) {
+TEST_F(SimpleOwnershipValidatorTest, VerifyExclusionCases) {
   bool hit = false;
-  size_t count = 0;
+  int count = -1;
+  auto fn = 
+    [&hit, &count](const std::vector<const type_info*>& violating) {
+      hit = true;
+      count = (int)violating.size();
+    };
 
-  std::shared_ptr<SimpleObject> obj;
+  std::shared_ptr<SimpleObject> obj(new SimpleObject);
+  std::weak_ptr<SimpleObject> objWeak(obj);
   {
     AutoCreateContext ctxt;
-    CurrentContextPusher pshr(ctxt);
     ctxt->EnforceSimpleOwnership();
-    ctxt->AddValidationListener(
-      [&hit, &count](const std::vector<const type_info*>& violating) {
-        hit = true;
-        count = violating.size();
-      }
-    );
+    ctxt->AddValidationListener(fn);
 
     // Add an arbitrary object to this context:
-    obj = AutoRequired<SimpleObject>();
+    ctxt->Add(obj);
   }
 
   // Verify we got hit, at a minimum:
   ASSERT_TRUE(hit) << "Validation listener was not invoked, ownership check was not enforced";
   ASSERT_EQ(1UL, count) << "An insufficient number of entities were detected as being incorrectly owned";
+
+  // Reset values, try again with another context:
+  hit = false;
+  count = -1;
+
+  {
+    AutoCreateContext ctxt;
+    ctxt->EnforceSimpleOwnership();
+    ctxt->AddValidationListener(fn);
+
+    // Add an arbitrary object to this context, but release the pointer this time:
+    ctxt->Add(obj);
+    obj.reset();
+  }
+
+  // Now verify that the object was destroyed as expected:
+  ASSERT_TRUE(objWeak.expired()) << "Weak pointer to enclosure object was not cleaned up correctly";
+  ASSERT_NE(0UL, count) << "A validation listener was invoked on a list of size zero, this is an error";
+  ASSERT_EQ(-1, count) << "A validation listener was invoked even though it should not have been called";
+  ASSERT_FALSE(hit) << "A validation listener was invoked when it should not have been invoked";
 }
