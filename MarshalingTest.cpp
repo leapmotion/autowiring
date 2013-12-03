8394b8b2b71c3830b2c320d9441fbea6664b0574
Jason Lokerson <cmercenary@gmail.com>
2013-12-03 15:20:39 -0800

    Tests broken up, validating some other behaviors of event streams

--- MarshalingTest.cpp
+++ MarshalingTest.cpp
@@ -11,37 +11,64 @@ public:
   virtual Deferred SampleEventDeferred(const std::string* str) = 0;
 };
 
-TEST_F(MarshalingTest, VerifySimpleSerialization) {
-  AutoFired<EventWithUuid> ewuuid;
-  EXPECT_FALSE(ewuuid.HasListeners()) << "A newly created event incorrectly reported that listeners existed where none were subscribed";
-
-  AutoCurrentContext ctxt;
-  std::shared_ptr<EventOutputStream> outputStream = ctxt->CreateEventOutputStream<EventWithUuid>();
-  ASSERT_NE(nullptr, outputStream.get()) << "Attempted to obtain an event stream from a context, but the returned pointer was null";
-
-  // Should be empty before we fire anything:
-  EXPECT_TRUE(outputStream->IsEmpty()) << "Output stream was empty, even though it was just created";
-  EXPECT_EQ(0UL, outputStream->GetSize()) << "Output stream reported " << outputStream->GetSize() << " bytes contained, it should have been empty";
-
-  // We DO have a listener, now that the output stream was created:
-  EXPECT_TRUE(ewuuid.HasListeners()) << "An output stream creation did not change the HasListeners disposition";
-
+void VerifyProperStreamReceipt(EventOutputStream* os, const AutoFired<EventWithUuid>& ewuuid) {
   // Test fire an event:
   std::string str("012345678900123456789012345678901234567890");
   ewuuid(&EventWithUuid::SampleEventFiring)(&str);
 
   // Verify that the output stream is no longer empty and has some default properties:
-  EXPECT_FALSE(outputStream->IsEmpty()) << "An output stream on an event was empty, even though an event it should have caught was just fired";
-  EXPECT_LT(0UL, outputStream->GetSize()) << "The output stream should have had more than zero bytes in it after recording an event, but it did not";
+  EXPECT_FALSE(os->IsEmpty()) << "An output stream on an event was empty, even though an event it should have caught was just fired";
+  EXPECT_LT(0UL, os->GetSize()) << "The output stream should have had more than zero bytes in it after recording an event, but it did not";
 
   // Verify that the output stream is _at least as long_ as the string argument we passed earlier:
-  EXPECT_LT(str.size(), outputStream->GetSize()) << "An output stream contained fewer uncompressed bytes than were transmitted in a fired event";
+  EXPECT_LT(str.size(), os->GetSize()) << "An output stream contained fewer uncompressed bytes than were transmitted in a fired event";
+}
 
-  // Flush the output stream and verify it does, in fact, reset its properties:
-  outputStream->Reset();
-  EXPECT_TRUE(outputStream->IsEmpty()) << "Output stream was not empty after being reset";
+TEST_F(MarshalingTest, VerifyListenersUpdated) {
+  AutoFired<EventWithUuid> ewuuid;
+  EXPECT_FALSE(ewuuid.HasListeners()) << "A newly created event incorrectly reported that listeners existed where none were subscribed";
+
+  AutoCurrentContext ctxt;
+  std::shared_ptr<EventOutputStream> os = ctxt->CreateEventOutputStream<EventWithUuid>();
+  ASSERT_NE(nullptr, os.get()) << "Attempted to obtain an event stream from a context, but the returned pointer was null";
+
+  // Should be listeners now:
+  EXPECT_TRUE(ewuuid.HasListeners()) << "An output stream creation did not change the HasListeners disposition";
 
   // Let the output stream fall out of scope and verify that the listener existence disposition is updated:
-  outputStream.reset();
+  os.reset();
+  EXPECT_FALSE(ewuuid.HasListeners()) << "An event incorrectly reported that it had listeners, even though its only listener--an output stream--is out of scope";
+}
+
+TEST_F(MarshalingTest, VerifyOutOfOrderFiring) {
+  // We should be able to create an output stream on an event even if nobody fires this event right now
+  AutoCurrentContext ctxt;
+  std::shared_ptr<EventOutputStream> os = ctxt->CreateEventOutputStream<EventWithUuid>();
+  ASSERT_NE(nullptr, os.get()) << "Failed to create an output stream in a context where no firers of the underlying event exist";
+
+  // Verify that we get stream reciept even if we fire at _this_ point, after the stream exists
+  VerifyProperStreamReceipt(os.get(), AutoFired<EventWithUuid>());
+}
+
+TEST_F(MarshalingTest, VerifySimpleSerialization) {
+  AutoFired<EventWithUuid> ewuuid;
+
+  AutoCurrentContext ctxt;
+  std::shared_ptr<EventOutputStream> os = ctxt->CreateEventOutputStream<EventWithUuid>();
+  ASSERT_NE(nullptr, os.get());
+
+  // Should be empty before we fire anything:
+  EXPECT_TRUE(os->IsEmpty()) << "Output stream was empty, even though it was just created";
+  EXPECT_EQ(0UL, os->GetSize()) << "Output stream reported " << os->GetSize() << " bytes contained, it should have been empty";
+
+  // Scoping behavior
+  VerifyProperStreamReceipt(os.get(), ewuuid);
+
+  // Flush the output stream and verify it does, in fact, reset its properties:
+  os->Reset();
+  EXPECT_TRUE(os->IsEmpty()) << "Output stream was not empty after being reset";
+
+  // Let the output stream fall out of scope and verify that the listener existence disposition is updated:
+  os.reset();
   EXPECT_FALSE(ewuuid.HasListeners()) << "An event incorrectly reported that it had listeners, even though its only listener--an output stream--is out of scope";
 }
