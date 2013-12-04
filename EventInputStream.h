6f51767ef8b88d3330699b15d4f166a38d049c46
Jason Lokerson <cmercenary@gmail.com>
2013-12-04 10:17:49 -0800

    Adding an input stream type and corresponding tests

---
+++ EventInputStream.h
@@ -0,0 +1,32 @@
+#pragma once
+
+/// <summary>
+/// Allows the deserialization of events from an output stream, in order to replay them in-process
+/// </summary>
+template<class T>
+class EventInputStream
+{
+public:
+  static_assert(std::is_base_of<EventReceiver, T>::value, "Cannot instantiate an event input stream on a non-event type");
+
+private:
+
+public:
+  /// <summary>
+  /// Enables a new event for firing, as a member of T, for transmission by this class
+  /// </summary>
+  template<class MemFn>
+  void Enable(MemFn memFn) {
+  }
+
+  /// <summary>
+  /// Interprets and fires a SINGLE EVENT from the passed input buffer
+  /// </summary>
+  /// <returns>
+  /// The number of bytes processed from the input buffer
+  /// </returns>
+  size_t FireSingle(const void* pData, size_t dataSize) const {
+    return 0;
+  }
+};
+
