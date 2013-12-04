57cc90de82ea986ca18591c68f3b73cc45db5e6e
codemercenary <jsanchez@leapmotion.com>
2013-12-04 08:57:27 -0800

    Commensurate elimination of PostHocBoltIntroduction, which is also no longer sense-bearing

--- SelfSelectingFixtureTest.cpp
+++ SelfSelectingFixtureTest.cpp
@@ -18,19 +18,3 @@ TEST_F(SelfSelectingFixtureTest, FixtureTest) {
   ASSERT_TRUE(ssf.IsAutowired()) << "Self-selecting fixture was not selected into a created context with the correct name";
   ASSERT_TRUE(ssf->IsMagicCorrect()) << "Self-selecting fixture was apparently not initialized correctly";
 }
-
-TEST_F(SelfSelectingFixtureTest, PostHocBoltIntroduction) {
-  // Create a context FIRST, and THEN create the bolt:
-  CurrentContextPusher pshr(m_create->Create("SelfSelect3"));
-  Autowired<SelfSelectingFixture> ssf;
-  ASSERT_FALSE(ssf.IsAutowired()) << "Fixture self-selected into a context prematurely";
-
-  // Now the bolt, this should introduce the fixture into the already-existing context
- // BOLT_TO(SelfSelectingFixture, "Global/IAMANON/SelfSelect3");
-  
-  // Autowired fields are funny--they can be satisfied at the point of their declaration, or if a member
-  // is added to a context which might satisfy an autowired field _after_ it is declared, they can be
-  // satisfied later.  This is called delay-satisfaction and it's necessary that bolted introductions of
-  // context members honor this property.
-  ASSERT_TRUE(ssf.IsAutowired());
-}
