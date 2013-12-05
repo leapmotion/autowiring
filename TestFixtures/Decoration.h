86f8748ba84ea137174fb3a4329605d8f676e529
Jason Lokerson <cmercenary@gmail.com>
2013-12-04 22:28:37 -0800

    Adding unit tests for the auto_out type by itself

--- TestFixtures/Decoration.h
+++ TestFixtures/Decoration.h
@@ -26,17 +26,25 @@ public:
   bool m_called;
   Decoration<0> m_zero;
   Decoration<1> m_one;
+  Decoration<2> m_two;
 };
 
 class FilterA:
   public FilterRoot
 {
 public:
+  FilterA(void) :
+    m_hitCount(0)
+  {}
+
   void AutoFilter(Decoration<0> zero, Decoration<1> one) {
     m_called = true;
     m_zero = zero;
     m_one = one;
+    m_hitCount++;
   }
+
+  size_t m_hitCount;
 };
 static_assert(has_autofilter<FilterA>::value, "Expected the filter to have an AutoFilter method");
 
@@ -123,4 +131,23 @@ public:
   void AutoFilter(const Decoration<0>& dec) {
     m_called = true;
   }
+};
+
+class FilterG:
+  public FilterRoot
+{
+public:
+  FilterG(void) :
+    m_hitCount(0)
+  {}
+
+  void AutoFilter(Decoration<0> zero, Decoration<1> one, Decoration<2> two) {
+    m_called = true;
+    m_zero = zero;
+    m_one = one;
+    m_two = two;
+    m_hitCount++;
+  }
+
+  size_t m_hitCount;
 };
