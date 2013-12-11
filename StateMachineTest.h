35db94f26cd842ebe056b922721f6f530c59e5a6
Graham Tremper <gtremper@gmail.com>
2013-12-11 10:03:53 -0800

    created sainity test. Testing boilerplate done

--- StateMachineTest.h
+++ StateMachineTest.h
@@ -1,11 +1,12 @@
 // Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
 #ifndef _CREATION_LISTENER_TEST_H
 #define _CREATION_LISTENER_TEST_H
-#include "EnclosedContextTestBase.h"
+#include <gtest/gtest.h>
 
-class CreationListenerTest:
-public EnclosedContextTestBase
+class StateMachineTest:
+public testing::Test
 {
 };
+
 
 #endif
