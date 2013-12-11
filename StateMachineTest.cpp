f8ec2f7aa6e786e5a2fc9a4ec7e51c5130e582cf
Graham Tremper <gtremper@gmail.com>
2013-12-11 10:03:53 -0800

    created sainity test. Testing boilerplate done

--- StateMachineTest.cpp
+++ StateMachineTest.cpp
@@ -1,4 +1,15 @@
 // Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
 #include "stdafx.h"
-#include "BoltTest.h"
+#include "StateMachine.h"
+#include "StateMachineTest.h"
+
+using namespace std;
+
+
+TEST_F(StateMachineTest, SanityTest) {
+  
+  
+  ASSERT_TRUE(true);
+}
+
 
