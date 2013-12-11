9ee85dfa6870fb6ad99308e5e70c1b8d1389f52e
Graham Tremper <gtremper@gmail.com>
2013-12-11 13:37:53 -0800

    basic version seems to work

--- StateMachine.cpp
+++ StateMachine.cpp
@@ -2,11 +2,31 @@
 #include "stdafx.h"
 #include "StateMachine.h"
 
-StateMachine::StateMachine(void)
+/*
+template<typename T_enum>
+StateMachine<>::StateMachine(T_enum startState)
 {
+  m_currentState = startState;
 }
 
-StateMachine::~StateMachine(void)
+StateMachine::~StateMachine()
 {
 
 }
+
+template<typename T_enum, typename T_input>
+T_enum StateMachine<T_enum>::transition(T_input input){
+  m_currentState = m_transitionMap[m_currentState];
+  return m_currentState;
+}
+
+template<typename T_enum, typename T_input>
+T_enum StateMachine<T_enum>::addTransition(T_enum state, lambda){
+  m_transitionMap[state] = lambda;
+}
+
+template<typename T_enum>
+T_enum StateMachine::getState(){
+  return m_currentState;
+}
+*/
--- StateMachine.h
+++ StateMachine.h
@@ -1,12 +1,35 @@
 // Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
 #ifndef _STATEMACHINE_H
 #define _STATEMACHINE_H
+#include <map>
 
+template<typename T_enum, typename T_input>
 class StateMachine
 {
+typedef T_enum (*t_func)(T_input);
 public:
-  StateMachine();
-  virtual ~StateMachine();
+  StateMachine(T_enum startState){
+    m_currentState = startState;
+  };
+  
+  virtual ~StateMachine(){};
+  
+  T_enum transition(T_input input){
+    m_currentState = m_transitionMap[m_currentState](input);
+    return m_currentState;
+  };
+  
+  void addTransition(T_enum state, t_func lambda){
+    m_transitionMap[state] = lambda;
+  };
+  
+  T_enum getState(){
+    return m_currentState;
+  };
+  
+private:
+  T_enum m_currentState;
+  std::map<T_enum, t_func> m_transitionMap;
 };
 
 #endif
