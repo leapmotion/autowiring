7a7ac754bbbcad19da968835b993d9519c3bd3ab
Jason Lokerson <cmercenary@gmail.com>
2013-12-11 14:52:29 -0800

    Altered concept for state machine definition and behavior attachment

--- StateMachine.h
+++ StateMachine.h
@@ -4,13 +4,60 @@
 #include <map>
 
 template<typename T_enum, typename T_input>
+struct TransitionObject {
+  TransitionObject(T_enum startState):
+    startState(startState)
+  {}
+
+private:
+  T_enum startState;
+
+public:
+  struct LhsWithTransition {
+    LhsWithTransition(TransitionObject& owner, T_enum lhs, T_input input):
+      owner(owner),
+      lhs(lhs),
+      input(input)
+    {}
+
+    TransitionObject& owner;
+    T_enum lhs;
+    T_input input;
+
+    TransitionObject&& operator<<(T_enum rhs) {
+      // TODO:  Inert the lhs, input, rhs trio into our owning transition object
+      return std::move(owner);
+    }
+  };
+
+  struct LhsState {
+    LhsState(TransitionObject& owner, T_enum lhs):
+      owner(owner),
+      lhs(lhs)
+    {}
+
+    TransitionObject& owner;
+    T_enum lhs;
+
+    LhsWithTransition operator<<(T_input input) {
+      return LhsWithTransition(owner, lhs, input);
+    }
+  };
+
+  LhsState operator<<(T_enum lhs) {
+    return LhsState(*this, lhs);
+  }
+};
+
+template<typename T_enum, typename T_input>
 class StateMachine
 {
 typedef T_enum (*t_func)(T_input);
 public:
-  StateMachine(T_enum startState){
-    m_currentState = startState;
-  };
+  StateMachine(TransitionObject<T_enum, T_input>&& tobj):
+    m_currentState(tobj.GetStartState())
+  {
+  }
   
   virtual ~StateMachine(){};
   
@@ -26,10 +73,25 @@ public:
   T_enum getState(){
     return m_currentState;
   };
-  
+
 private:
   T_enum m_currentState;
   std::map<T_enum, t_func> m_transitionMap;
+
+protected:
+  static TransitionObject<T_enum, T_input> machine(T_enum startState) {
+    return TransitionObject<T_enum, T_input>(startState);
+  }
+
+  // Operator overloads for behavior introduction:
+  template<class T>
+  StateMachine& operator+=(void (T::*memFn)()) {
+    return *this;
+  }
+
+  StateMachine& operator+=(std::function<void()>&& rhs) {
+    return *this;
+  }
 };
 
 #endif
