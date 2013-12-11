6a16370f33a1adc029093ce381cb19f9b817c6c8
Graham Tremper <gtremper@gmail.com>
2013-12-11 13:37:53 -0800

    basic version seems to work

--- StateMachineTest.cpp
+++ StateMachineTest.cpp
@@ -6,10 +6,41 @@
 using namespace std;
 
 
-TEST_F(StateMachineTest, SanityTest) {
+TEST_F(StateMachineTest, SimpleStates) {
   
+  enum class states {BLUE, GREEN, RED, BAD_STATE};
   
-  ASSERT_TRUE(true);
+  StateMachine<states,int> machine((states::BLUE));
+  
+  auto trans = [](int i){
+    switch (i){
+      case 0:
+        return states::BLUE;
+        break;
+      case 1:
+        return states::GREEN;
+        break;
+      case 2:
+        return states::RED;
+        break;
+    }
+    return states::BAD_STATE;
+  };
+  
+  machine.addTransition(states::BLUE, trans);
+  machine.addTransition(states::GREEN, trans);
+  machine.addTransition(states::RED, trans);
+  
+  EXPECT_EQ(states::BLUE, machine.getState());
+  
+  machine.transition(2);
+  
+  EXPECT_EQ(states::RED, machine.getState());
+
+  machine.transition(1);
+  
+  EXPECT_EQ(states::GREEN, machine.getState());
+  
 }
 
 
