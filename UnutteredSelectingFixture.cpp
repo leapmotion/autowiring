70060ef943e483efa90171f7d7a14590e79bae3a
Jason Lokerson <cmercenary@gmail.com>
2013-12-10 18:16:03 -0800

    Added test to detect overly aggressive symbol elimination

---
+++ UnutteredSelectingFixture.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "UnutteredSelectingFixture.h"
+#include "MicroBolt.h"
+
+BOLT_TO(SelfSelect, UnutteredSelectingFixture)
+
+UnutteredSelectingFixture::UnutteredSelectingFixture()
+{
+}
+
+UnutteredSelectingFixture::~UnutteredSelectingFixture()
+{
+}
---
+++ UnutteredSelectingFixture.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "ContextMember.h"
+
+struct SelfSelect {};
+
+class UnutteredSelectingFixture:
+  public ContextMember
+{
+public:
+  UnutteredSelectingFixture(void);
+  ~UnutteredSelectingFixture(void);
+};
+
