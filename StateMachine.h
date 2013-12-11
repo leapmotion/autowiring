6f3d9bd3e51d821c535a934b720484d23db39c29
Graham Tremper <gtremper@gmail.com>
2013-12-10 17:51:14 -0800

    added files for state machine

---
+++ StateMachine.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
+#ifndef _STATEMACHINE_H
+#define _STATEMACHINE_H
+
+class StateMachine
+{
+public:
+  StateMachine();
+  virtual ~StateMachine();
+};
+
+#endif
---
+++ StateMachineTest.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
+#ifndef _CREATION_LISTENER_TEST_H
+#define _CREATION_LISTENER_TEST_H
+#include "EnclosedContextTestBase.h"
+
+class CreationListenerTest:
+public EnclosedContextTestBase
+{
+};
+
+#endif
