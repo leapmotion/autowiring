b1022179639efd8601f464d9c8c42ca57f8bd667
Graham Tremper <gtremper@gmail.com>
2013-12-10 17:51:14 -0800

    added files for state machine

---
+++ StateMachine.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
+#include "stdafx.h"
+#include "StateMachine.h"
+
+StateMachine::StateMachine(void)
+{
+}
+
+StateMachine::~StateMachine(void)
+{
+
+}
