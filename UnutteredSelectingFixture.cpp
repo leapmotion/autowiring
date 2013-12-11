87c86da9a61c4e4750266a2c0e17d6b800e66067
codemercenary <jsanchez@leapmotion.com>
2013-12-11 14:24:46 -0800

    Intermediate commit

--- UnutteredSelectingFixture.cpp
+++ UnutteredSelectingFixture.cpp
@@ -4,7 +4,30 @@
 
 struct SelfSelect {};
 
-BOLT_TO(SelfSelect, UnutteredSelectingFixture)
+BOLT_TO(SelfSelect, UnutteredSelectingFixture);
+
+template<class S, class I, S from, I input, S to>
+struct Transition {};
+
+enum PlaybackStates {
+  Stopped,
+  Paused,
+  Running
+};
+
+enum Commands {
+  Play,
+  Stop,
+  Pause
+};
+
+template<>
+struct Transition<PlaybackStates, Commands, Stopped, Play, Running> {};
+
+class ComdatClass {
+public:
+  ComdatClass(void) {};
+};
 
 UnutteredSelectingFixture::UnutteredSelectingFixture()
 {
