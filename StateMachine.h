427f0d1e167dffc52a3070b85e035964fb8aa8f1
Graham Tremper <gtremper@gmail.com>
2013-12-11 16:01:18 -0800

    fixed syntax errors

--- StateMachine.h
+++ StateMachine.h
@@ -1,86 +1,90 @@
 // Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
 #ifndef _STATEMACHINE_H
 #define _STATEMACHINE_H
+#include "stdafx.h"
 #include <map>
+#include <utility>
+#include FUNCTIONAL_HEADER
+#include RVALUE_HEADER
 
-template<typename T_enum, typename T_input>
+template<typename T_states, typename T_input>
 struct TransitionObject {
-  TransitionObject(T_enum startState):
+  TransitionObject(T_states startState):
     startState(startState)
   {}
 
 private:
-  T_enum startState;
+  T_states startState;
 
 public:
   struct LhsWithTransition {
-    LhsWithTransition(TransitionObject& owner, T_enum lhs, T_input input):
+    LhsWithTransition(TransitionObject& owner, T_states lhs, T_input input):
       owner(owner),
       lhs(lhs),
       input(input)
     {}
 
     TransitionObject& owner;
-    T_enum lhs;
+    T_states lhs;
     T_input input;
 
-    TransitionObject&& operator<<(T_enum rhs) {
+    TransitionObject&& operator<<(T_states rhs) {
       // TODO:  Inert the lhs, input, rhs trio into our owning transition object
       return std::move(owner);
     }
   };
 
   struct LhsState {
-    LhsState(TransitionObject& owner, T_enum lhs):
+    LhsState(TransitionObject& owner, T_states lhs):
       owner(owner),
       lhs(lhs)
     {}
 
     TransitionObject& owner;
-    T_enum lhs;
+    T_states lhs;
 
     LhsWithTransition operator<<(T_input input) {
       return LhsWithTransition(owner, lhs, input);
     }
   };
 
-  LhsState operator<<(T_enum lhs) {
+  LhsState operator<<(T_states lhs) {
     return LhsState(*this, lhs);
   }
 };
 
-template<typename T_enum, typename T_input>
+template<typename T_states, typename T_input>
 class StateMachine
 {
-typedef T_enum (*t_func)(T_input);
+typedef std::function<T_states(T_input)> t_func;
 public:
-  StateMachine(TransitionObject<T_enum, T_input>&& tobj):
+  StateMachine(TransitionObject<T_states, T_input>&& tobj):
     m_currentState(tobj.GetStartState())
   {
   }
   
   virtual ~StateMachine(){};
   
-  T_enum transition(T_input input){
+  T_states transition(T_input input){
     m_currentState = m_transitionMap[m_currentState](input);
     return m_currentState;
   };
   
-  void addTransition(T_enum state, t_func lambda){
+  void addTransition(T_states state, t_func lambda){
     m_transitionMap[state] = lambda;
   };
   
-  T_enum getState(){
+  T_states getState(){
     return m_currentState;
   };
 
 private:
-  T_enum m_currentState;
-  std::map<T_enum, t_func> m_transitionMap;
+  T_states m_currentState;
+  std::map<T_states, t_func> m_transitionMap;
 
 protected:
-  static TransitionObject<T_enum, T_input> machine(T_enum startState) {
-    return TransitionObject<T_enum, T_input>(startState);
+  static TransitionObject<T_states, T_input> machine(T_states startState) {
+    return TransitionObject<T_states, T_input>(startState);
   }
 
   // Operator overloads for behavior introduction:
