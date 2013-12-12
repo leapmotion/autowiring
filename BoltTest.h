00b7d4097441b258c368b012d4c0fe916b8d4b3b
Jason Lokerson <cmercenary@gmail.com>
2013-12-11 22:13:20 -0800

    Updating test name to reflect the file it's in

--- BoltTest.h
+++ BoltTest.h
@@ -1,11 +1,8 @@
-// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
-#ifndef _CREATION_LISTENER_TEST_H
-#define _CREATION_LISTENER_TEST_H
+#pragma once
 #include "EnclosedContextTestBase.h"
 
-class CreationListenerTest:
+class BoltTest:
   public EnclosedContextTestBase
 {
 };
 
-#endif
