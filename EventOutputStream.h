6537cba9e6f8c68c95141b147e15fac7a4f2b378
Max Sills <sillsm@gmail.com>
2013-12-16 02:23:03 -0800

    2nd test close to passing. Need to implement << checking SFINAE

--- EventOutputStream.h
+++ EventOutputStream.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "Decompose.h"
 #include <memory>
+#include <iostream>
+#include <sstream>
+
+
+
+
 
 class EventOutputStreamBase {
+private:
+  std::stringstream m_OutputStream;
 public:
   EventOutputStreamBase(void);
   virtual ~EventOutputStreamBase(void);
@@ -26,6 +34,25 @@ public:
   /// Resets the output stream, preparing it for new writes.  Does not release or reallocate memory.
   /// </summary>
   void Reset(void);
+  
+  /// <summary>
+  /// Returns true if memfn is enabled, otherwise false.
+  /// </summary>
+  template<class MemFn>
+  bool IsEnabled(MemFn eventIden, bool amIRegistered = false) {
+	static int registration = 0; //first time func is checked
+	if (amIRegistered && registration < 1){
+    registration++;
+	}
+	return (!!registration);
+  }
+  
+  template <class Memfn, class Arg1>
+  void Serialize(Memfn & memfn, Arg1 & arg1){
+	  std::cout <<  "Hi! Just tried to serialize this stuff" << std::endl;
+	  m_OutputStream << arg1;
+	  std::cout << "From my outputs" << m_OutputStream << std::endl;
+  }
 };
 
 /// <summary>
@@ -41,6 +68,7 @@ public:
   EventOutputStream(){}
   ~EventOutputStream(){}
   void New(){}
+
   /// <summary>
   /// Enables a new event for serialization via its identity
   /// </summary>
@@ -48,6 +76,9 @@ public:
   void EnableIdentity(MemFn eventIden) {
     // We cannot serialize an identity we don't recognize
     static_assert(std::is_same<typename Decompose<MemFn>::type, T>::value, "Cannot add a member function unrelated to the output type for this class");
+	IsEnabled(eventIden, true);
   }
+
+
 };
 
