f2625ddad5e981b5d1f02c5b58cd3f57c7e26773
codemercenary <jsanchez@leapmotion.com>
2013-12-18 00:42:49 -0800

    Refactored the EventReceiverProxy class name to JunctionBox

--- EventReceiver.h
+++ EventReceiver.h
@@ -1,6 +1,4 @@
-// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
-#ifndef _EVENT_RECEIVER_H
-#define _EVENT_RECEIVER_H
+#pragma once
 #include <assert.h>
 
 class CoreThread;
@@ -50,4 +48,3 @@ public:
   }
 };
 
-#endif
