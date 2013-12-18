fbf7ed4cb16991e0020fb1b60a76ed24b6d2db06
Jason Lokerson <cmercenary@gmail.com>
2013-12-18 11:43:16 -0800

    Adding a unit test to ensure peer context transitivity

--- PeerContextTest.cpp
+++ PeerContextTest.cpp
@@ -1,9 +1,11 @@
 #include "stdafx.h"
 #include "PeerContextTest.h"
 #include "CoreThread.h"
+#include "TestFixtures/SimpleObject.h"
 #include "TestFixtures/SimpleReceiver.h"
 
-struct PeerContextName {};
+struct PeerContextName1 {};
+struct PeerContextName2 {};
 
 TEST_F(PeerContextTest, VerifySimplePeerage) {
   // Insert a simple receiver first:
@@ -21,7 +23,7 @@ TEST_F(PeerContextTest, VerifySimplePeerage) {
 
   {
     // Create a single peer context, make it current, and try to fire:
-    auto peer = m_create->CreatePeer<PeerContextName>();
+    auto peer = m_create->CreatePeer<PeerContextName1>();
     ASSERT_NE(nullptr, peer.get()) << "Peer context creation method returned a null pointer";
 
     CurrentContextPusher pshr(peer);
@@ -33,4 +35,26 @@ TEST_F(PeerContextTest, VerifySimplePeerage) {
   // was fired from another context:
   EXPECT_TRUE(sr->m_one) << "Simple reciever did not receive an event originating in a peer context";
   EXPECT_EQ(22, sr->m_oneArg) << "Simple reciever did not recieve the correct argument from a peer context event";
+}
+
+TEST_F(PeerContextTest, VerifyPeerTransitivity) {
+  // Insert our simple receiver:
+  AutoRequired<SimpleReceiver> sr;
+
+  // Now create the first peer context:
+  auto peer1 = m_create->CreatePeer<PeerContextName1>();
+  ASSERT_NE(nullptr, peer1.get());
+
+  // Create the _second_ peer context based on the first:
+  auto peer2 = peer1->CreatePeer<PeerContextName2>();
+  ASSERT_NE(nullptr, peer2.get());
+
+  // Fire the event here:
+  CurrentContextPusher pshr(peer2);
+  AutoFired<CallableInterface> ci;
+  ci(&CallableInterface::OneArg)(25);
+
+  // Verify that the event receiver in the base context managed to get the message:
+  ASSERT_TRUE(sr->m_one) << "Simple receiver did not properly recieve an event from a transitively peered context";
+  ASSERT_EQ(25, sr->m_oneArg) << "Simple receiver properly recieved a transitively peered context's event, but its argument was incorrect";
 }
