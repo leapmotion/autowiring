6c10af27e0f3c4d2a991c370d8b4b0f939848db2
Graham Tremper <gtremper@gmail.com>
2013-12-19 17:13:16 -0800

    created JunctionBoxManager class

---
+++ JunctionBoxManager.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
+#include "stdafx.h"
+#include "JunctionBoxManager.h"
+
+JunctionBoxManager::JunctionBoxManager(void) {
+}
+
+JunctionBoxManager::~JunctionBoxManager(void) {
+}
