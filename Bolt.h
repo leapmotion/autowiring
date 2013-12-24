b21521fe197193ed3ad67f4f8ab3c8089ba1ac82
Walter Gray <wgray@leapmotion.com>
2013-12-23 19:05:41 -0800

    Fixed some docs, added another snooptest
    Revided HardwareStateBolt & PipelineCreationBolt to use Boltable as they should.

--- Bolt.h
+++ Bolt.h
@@ -7,6 +7,9 @@
 /// <param name="Sigil">The sigil of the context whose creation we want to listen for</param>
 /// <remarks>
 /// A bolt is a way to augment the creation of a new context in a declarative, by-name manner.
+/// By inheriting from Bolt, the object will recieve ContextCreated() calls.
+/// To create a class that will have a new instance inserted into each instance of a context
+/// with a given sigil, use Boltable.
 /// </remarks>
 template<class Sigil>
 class Bolt:
