006c488777878ba35eded947fa539439dd12e33d
Max Sills <msills@leapmotion.com>
2013-12-27 05:40:43 -0500

    Addition of the typedef dector for arg deduction. All tests pass.

--- EventInputStream.h
+++ EventInputStream.h
@@ -11,24 +11,33 @@
 #define EnableIdentity(x) SpecialAssign<decltype(x), x> (#x) 
 #endif
 
+template <int n>
+struct CompileTimeInt{};
+
 //The point here is to use template specialization to pick default-deserialization for certain types at registration 
+
 template <typename T>
 struct DeserializeHelper{
    static T Deserialize(std::string & str){
     T arg1;
     std::stringstream buf;
-    buf << s1;
+    buf << str;
     buf >> arg1;
-    return arg1
+    return arg1;
   }
 };
+
 //The point here is to use template specialization to pick default-deserialization for certain types at registration 
 template <typename T>
 struct DeserializeHelper<const T * >{
-  static const T * Deserialize(std::string & str){
+  const T * Deserialize(std::string & str){
     const T * ret = &str;
     return ret;
   }
+};
+
+struct NoDo{
+   CompileTimeInt<666> * Deserialize(std::string & str){return nullptr;}	
 };
 
 /// <summary>
@@ -54,6 +63,44 @@ struct DeferOrFire<T, MemFn, void>
   {
     return t(memfn);
   }
+};
+
+/// <summary>
+/// N-Member typedef detector
+/// Reflection primitive which deduces 2^4 possible types from Decompose(memfn)
+/// Use: auto arg1 = NMemberDetector::select<Test>(nullptr, "12").Deserialize(xx);
+/// </summary>
+struct TypedefDetector
+{
+  template<class U>
+  static DeserializeHelper<typename U::t_arg1> select(typename U::t_arg1 *, CompileTimeInt<1>)
+  {
+  return DeserializeHelper<typename U::t_arg1>();
+  }
+  
+  template<class U>
+  static DeserializeHelper<typename U::t_arg2> select(typename U::t_arg2 *, CompileTimeInt<2>)
+  {
+  return DeserializeHelper<typename U::t_arg2>();
+  }  
+  
+  template<class U>
+  static DeserializeHelper<typename U::t_arg3> select(typename U::t_arg3 *, CompileTimeInt<3>)
+  {
+  return DeserializeHelper<typename U::t_arg3>();
+  }  
+  
+  template<class U>
+  static DeserializeHelper<typename U::t_arg4> select(typename U::t_arg4 *, CompileTimeInt<4>)
+  {
+  return DeserializeHelper<typename U::t_arg4>();
+  }
+
+  template<class U>
+  static NoDo select(...){
+  return NoDo();
+  }
+
 };
 
 /// <summary>
@@ -86,47 +133,14 @@ struct Expression<T, Memfn, memfn, 1>: public ExpressionBase{
     void func(std::string s1 = "", std::string s2 = "", std::string s3= "", std::string s4 = ""){      
     AutoFired<T> sender;
     DeferOrFire<decltype(sender), Memfn, typename Decompose<Memfn>::retType> FireType;
-    auto arg1 = DeserializeHelper<typename Decompose<Memfn>::t_arg1>::Deserialize(s1);
+    //auto arg1 = DeserializeHelper<typename Decompose<Memfn>::t_arg1>::Deserialize(s1);
+    auto arg1 = TypedefDetector::select<typename Decompose<Memfn> >(nullptr, CompileTimeInt<1>()).Deserialize(s1);
+    auto arg2 = TypedefDetector::select<typename Decompose<Memfn>>(nullptr, CompileTimeInt<2>()).Deserialize(s2);
+    auto arg3 = TypedefDetector::select<typename Decompose<Memfn>>(nullptr, CompileTimeInt<3>()).Deserialize(s3);
+    auto arg4 = TypedefDetector::select<typename Decompose<Memfn>>(nullptr, CompileTimeInt<4>()).Deserialize(s4);
     FireType.func(sender, memfn)(arg1);
   }
 };
-
-template <class T, class Memfn, Memfn memfn>
-struct Expression<T, Memfn, memfn, 2>: public ExpressionBase{
-    void func(std::string s1 = "", std::string s2 = "", std::string s3= "", std::string s4 = ""){      
-    AutoFired<T> sender;
-    DeferOrFire<decltype(sender), Memfn, typename Decompose<Memfn>::retType> FireType;
-    auto arg1 = DeserializeHelper<typename Decompose<Memfn>::t_arg1>::Deserialize(s1);
-    auto arg2 = DeserializeHelper<typename Decompose<Memfn>::t_arg2>::Deserialize(s2);
-    FireType.func(sender, memfn)(arg1, arg2);
-  }
-};
-
-template <class T, class Memfn, Memfn memfn>
-struct Expression<T, Memfn, memfn, 3>: public ExpressionBase{
-    void func(std::string s1 = "", std::string s2 = "", std::string s3= "", std::string s4 = ""){      
-    AutoFired<T> sender;
-    DeferOrFire<decltype(sender), Memfn, typename Decompose<Memfn>::retType> FireType;
-    auto arg1 = DeserializeHelper<typename Decompose<Memfn>::t_arg1>::Deserialize(s1);
-    auto arg2 = DeserializeHelper<typename Decompose<Memfn>::t_arg2>::Deserialize(s2);
-    auto arg3 = DeserializeHelper<typename Decompose<Memfn>::t_arg3>::Deserialize(s3);
-    FireType.func(sender, memfn)(arg1, arg2, arg3);
-  }
-};
-
-template <class T, class Memfn, Memfn memfn>
-struct Expression<T, Memfn, memfn, 4>: public ExpressionBase{
-    void func(std::string s1 = "", std::string s2 = "", std::string s3= "", std::string s4 = ""){      
-    AutoFired<T> sender;
-    DeferOrFire<decltype(sender), Memfn, typename Decompose<Memfn>::retType> FireType;
-    auto arg1 = DeserializeHelper<typename Decompose<Memfn>::t_arg1>::Deserialize(s1);
-    auto arg2 = DeserializeHelper<typename Decompose<Memfn>::t_arg2>::Deserialize(s2);
-    auto arg3 = DeserializeHelper<typename Decompose<Memfn>::t_arg3>::Deserialize(s3);
-    auto arg3 = DeserializeHelper<typename Decompose<Memfn>::t_arg4>::Deserialize(s4);
-    FireType.func(sender, memfn)(arg1, arg2, arg3, arg4);
-  }
-};
-
 
 /// <summary>
 /// Allows the deserialization of events from an output stream, in order to replay them in-process
