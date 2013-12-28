b45574c0c5ebc9166461d1cf240a2a7daab6d3b8
codemercenary <jsanchez@leapmotion.com>
2013-12-28 10:29:11 -0800

    Adding a global count to make allocation easier

--- TypeRegistry.cpp
+++ TypeRegistry.cpp
@@ -2,10 +2,12 @@
 #include "TypeRegistry.h"
 
 const TypeRegistryEntry* g_pFirstEntry = nullptr;
+size_t g_entryCount = 0;
 
 TypeRegistryEntry::TypeRegistryEntry(const std::type_info& ti) :
   pFlink(g_pFirstEntry),
   ti(ti)
 {
+  g_entryCount++;
   g_pFirstEntry = this;
 }
