3a152cd109aac182b5ebc2e351920176f208ad91
Graham Tremper <gtremper@gmail.com>
2013-12-30 10:07:41 -0800

    Added factory for junctionBox in type registry

--- TypeRegistry.cpp
+++ TypeRegistry.cpp
@@ -4,9 +4,10 @@
 const TypeRegistryEntry* g_pFirstEntry = nullptr;
 size_t g_entryCount = 0;
 
-TypeRegistryEntry::TypeRegistryEntry(const std::type_info& ti) :
+TypeRegistryEntry::TypeRegistryEntry(const std::type_info& ti, std::function<JunctionBoxBase*(void)> factory) :
+  ti(ti),
   pFlink(g_pFirstEntry),
-  ti(ti)
+  m_pFactory(factory)
 {
   g_entryCount++;
   g_pFirstEntry = this;
