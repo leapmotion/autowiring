f9fd7c90b5ed201a01f13fb4597105281e485af6
Walter Gray <wgray@leapmotion.com>
2014-01-24 19:17:18 -0800

    Added a test demonstrating desired functionality regarding referencing members inside of a context's teardown notification.

--- TeardownNotifierTest.cpp
+++ TeardownNotifierTest.cpp
@@ -19,3 +19,27 @@ TEST_F(TeardownNotifierTest, VerifySingleNotification) {
 
   EXPECT_TRUE(hit) << "Teardown listener was not hit as expected during context teardown";
 }
+
+TEST_F(TeardownNotifierTest, ReferenceMemberInTeardown) {
+
+  bool hit = false;
+  {
+    AutoCreateContext ctxt;
+    CurrentContextPusher pshr(ctxt);
+
+    AutoRequired <SimpleMember> member;
+
+    std::weak_ptr<CoreContext> ctxtWeak;
+    ctxt->AddTeardownListener[ctxtWeak] {
+      try
+      {
+        Autowired<SimpleMember> member;
+        if (member)
+          hit = true;
+      }
+      catch (autowiring_error) {}
+    });
+  }
+
+  EXPECT_TRUE(hit) << "Failed to reference a member of a context in it's teardown listener"
+}
