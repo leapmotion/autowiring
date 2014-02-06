dea0975bb6bdd32705fb02f122e7cfd8da12bd71
Kevin Horowitz <khorowitz@leapmotion.com>
2014-02-06 13:35:58 -0800

    Fixed unused file warning.

--- StateMachine.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
-#include "stdafx.h"
-#include "StateMachine.h"
-
-/*
-Everything is in the header. For now...
-*/
