1e5997062edfc0d72a9d65eb2a5f1824ad859d54
Jason Lokerson <cmercenary@gmail.com>
2014-02-12 12:54:33 -0800

    Adding stub tests for context anchors
    Conflicts:
    	source/Autowiring/Autowired.h

--- CoreContext.h
+++ CoreContext.h
@@ -59,6 +59,18 @@ class Autowired;
 template<class Sigil1, class Sigil2, class Sigil3>
 struct Boltable;
 
+/// <summary>
+/// Anchors a particular object type or event type to the annotated context sigil
+/// </summary>
+/// <remarks>
+/// The AutoAnchor is a marker type to be applied to a sigil type.  When a member of a subcontext of any
+/// annotated sigil class attempts to AutoRequire or AutoFire an instance of any sigil type, instead of
+/// creating the corresponding object or obtaining the junction box in that child context, the request
+/// will be satisfied instead by the anchor.
+/// </remarks>
+template<class... Ts>
+struct AutoAnchor {};
+
 #define CORE_CONTEXT_MAGIC 0xC04EC0DE
 
 enum class ShutdownMode {
