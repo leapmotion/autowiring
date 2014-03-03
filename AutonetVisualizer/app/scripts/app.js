cefc1b88d58834208638169e2b40c66f87b3d52e
Graham Tremper <gtremper@gmail.com>
2014-03-03 11:33:08 -0800

    Added AutonetVisualizer

---
+++ AutonetVisualizer/app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('autoNetApp', [
+  'ngRoute'
+])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
