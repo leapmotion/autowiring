934c3cfed55a4b6d9746d5d4b42969bd0023d5dd
Jason Lokerson <cmercenary@gmail.com>
2014-03-03 13:40:33 -0800

    Adding unit tests for graceful shutdown

--- ContextCleanupTest.cpp
+++ ContextCleanupTest.cpp
@@ -102,7 +102,7 @@ TEST_F(ContextCleanupTest, VerifyThreadCleanup) {
   CurrentContextPusher pshr(context);
 
   // Add a simple thread object
-  context->Inject<SimpleThreaded>();
+  AutoRequired<SimpleThreaded>();
 
   // Kick off the operation
   context->InitiateCoreThreads();
@@ -134,6 +134,23 @@ public:
 
   bool m_notified;
 };
+
+TEST_F(ContextCleanupTest, VerifyGracefulThreadCleanup) {
+  m_create->InitiateCoreThreads();
+  AutoRequired<CoreThread> ct;
+  ct->DelayUntilCanAccept();
+
+  // Just create a CoreThread directly and have it pend some lambdas that will take awhile to run:
+  auto called = std::make_shared<bool>(false);
+  *ct += [] {
+    boost::this_thread::sleep_for(boost::chrono::milliseconds(100));
+  };
+  *ct += [called] { *called = true; };
+
+  // Verify that a graceful shutdown ensures both lambdas are called:
+  m_create->SignalShutdown(true, ShutdownMode::Graceful);
+  ASSERT_TRUE(*called) << "Graceful shutdown did not correctly run down all lambdas";
+}
 
 TEST_F(ContextCleanupTest, VerifyNotificationReciept) {
   std::shared_ptr<ReceivesTeardownNotice> rtn;
