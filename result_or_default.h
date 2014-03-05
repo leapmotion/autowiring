ae072940371d3b5fb16e6414c05c367ba324c9a8
codemercenary <jsanchez@leapmotion.com>
2014-03-05 10:01:54 -0800

    Adding a result_or_default class which provides function traits support for return type defaulting

---
+++ result_or_default.h
@@ -0,0 +1,31 @@
+#pragma once
+
+/// <param name="fn">The function to be called</param>
+/// <param name="args">The arguments to be passed to the function</param>
+/// <returns>
+/// The result of fn if decltype(fn(args...)) is the same as T, otherwise default
+/// </returns>
+template<class T, class Fn, class... Args>
+typename std::enable_if<
+  std::is_same<
+    T,
+    typename std::result_of<Fn(Args...)>::type
+  >::value,
+  T
+>::type
+result_or_default(Fn& fn, T default, Args&&... args) {
+  return fn(std::forward<Args>(args)...);
+}
+
+template<class T, class Fn, class... Args>
+typename std::enable_if<
+  !std::is_same<
+    T,
+    typename std::result_of<Fn(Args...)>::type
+  >::value,
+  T
+>::type
+result_or_default(Fn& fn, T default, Args&&... args) {
+  fn(std::forward<Args>(args)...);
+  return default;
+}
