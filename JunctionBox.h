d0cfec67d81ced71bb069b347e8b36fed863d797
Jason Lokerson <cmercenary@gmail.com>
2014-03-11 21:09:55 -0700

    Eliminating an overload that creates unnecessary ambiguity

--- JunctionBox.h
+++ JunctionBox.h
@@ -348,7 +348,7 @@ public:
     for(auto q = dq.begin(); q != dq.end(); q++)
       if((**q).CanAccept())
         // Create a fully curried function to add to the dispatch queue:
-        (**q) += static_cast<DispatchThunkBase*>(new CurriedInvokeRelay<T, Args...>(dynamic_cast<T&>(**q), fnPtr, args...));
+        (**q).AddExisting(new CurriedInvokeRelay<T, Args...>(dynamic_cast<T&>(**q), fnPtr, args...));
   }
 };
 
