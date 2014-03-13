6f9999a6496aea17aa34f228d6fcd8046d6f6a41
Graham Tremper <gtremper@leapmotion.com>
2014-03-13 14:35:37 -0700

    Added more documentation

--- AutonetVisualizer/app/scripts/controllers/header.js
+++ AutonetVisualizer/app/scripts/controllers/header.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Handles the buisness logic for the header and subscribe/unsubscribe buttons
 angular.module('autoNetApp')
 .controller('HeaderCtrl', ['$scope', 'websocket', function ($scope, websocket) {
   $scope.isSubscribed = false;
