a14a9ba4c018fcf10e829457a9e69d904980ba5f
Jason Lokerson <cmercenary@gmail.com>
2014-03-17 17:43:38 -0700

    Moving AutoAnchor into its own header file

---
+++ AutoAnchor.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Decompose.h"
+#include <set>
+#include TYPE_INDEX_HEADER
+
+/// <summary>
+/// Anchors a particular object type or event type to the annotated context sigil
+/// </summary>
+/// <remarks>
+/// The AutoAnchor is a marker type to be applied to a sigil type.  When a member of a subcontext of any
+/// annotated sigil class attempts to AutoRequire or AutoFire an instance of any sigil type, instead of
+/// creating the corresponding object or obtaining the junction box in that child context, the request
+/// will be satisfied instead by the anchor.
+/// </remarks>
+struct AutoAnchorBase {
+  static void Enumerate(std::set<std::type_index>& anchors) {
+    assert(false); // Base should never be called
+  }
+};
+
+template<typename... Ts>
+struct AutoAnchor:
+  AutoAnchorBase
+{
+  static_assert(!is_any_same<void, Ts...>::value, "Can't use 'void' as a sigil type");
+  static void Enumerate(std::set<std::type_index>& anchors) {
+    bool dummy[] = {
+      (anchors.insert(typeid(Ts)), false)...
+    };
+    (void) dummy;
+  }
+};
