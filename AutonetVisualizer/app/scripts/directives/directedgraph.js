6c613f9d3bfb9d947a51aa992fd99cc1c4d0c02c
Graham Tremper <gtremper@leapmotion.com>
2014-03-20 11:43:43 -0700

    Small changes

--- AutonetVisualizer/app/scripts/directives/directedgraph.js
+++ AutonetVisualizer/app/scripts/directives/directedgraph.js
@@ -8,7 +8,7 @@
 angular.module('autoNetApp')
 .directive('directedGraph', function () {
   return {
-    template: '<canvas width="900" height="400">HTML5 canvas not supported on your browser</canvas>',
+    template: '<canvas width="1100" height="450">HTML5 canvas not supported on your browser</canvas>',
     restrict: 'E',
     replace: true,
     scope: {
