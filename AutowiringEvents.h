b25a7bf92285dff4c24a501a646ad4a6f41e1ea7
Graham Tremper <gtremper@gmail.com>
2014-03-20 11:14:20 -0700

    Expired contexts now send events to Autonet server

--- AutowiringEvents.h
+++ AutowiringEvents.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "EventReceiver.h"
-#include SHARED_PTR_HEADER
 
 class CoreContext;
 
@@ -8,6 +7,6 @@ class AutowiringEvents:
   public virtual EventReceiver
 {
 public:
-  virtual void NewContext(std::shared_ptr<CoreContext>)=0;
-  virtual void ExpiredContext(std::shared_ptr<CoreContext>)=0;
+  virtual void NewContext(CoreContext&)=0;
+  virtual void ExpiredContext(CoreContext&)=0;
 };
