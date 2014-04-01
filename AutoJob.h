43cbbf42d1d8b786ddc824f219914b8b4298a857
Graham Tremper <gtremper@gmail.com>
2014-04-01 13:32:44 -0700

    Made some EventDispatcher functions protected.

--- AutoJob.h
+++ AutoJob.h
@@ -10,9 +10,19 @@ public:
   AutoJob(const char* name = nullptr);
 
 private:
+  boost::mutex m_lock;
+  boost::condition_variable m_stateCondition;
+  
+  // Acceptor flag:
+  bool m_canAccept;
 
 public:
   // EventDispatcher overrides:
   bool DelayUntilCanAccept(void) override;
   bool CanAccept(void) const override;
+  
+  // "CoreThread" stuff
+  bool ShouldStop(void) const;
+  void AcceptDispatchDelivery(void);
+  void RejectDispatchDelivery(void);
 };
