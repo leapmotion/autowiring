41631b7d37e916cd0153dc78ab8a323ed434fe98
codemercenary <jsanchez@leapmotion.com>
2014-04-10 02:32:17 -0700

    type_index is inappropriate in this context; should be a pointer to the type_info itself

--- AutowiringEvents.h
+++ AutowiringEvents.h
@@ -18,6 +18,13 @@ public:
   virtual void NewContextMember(const ContextMember&)=0;
   virtual void NewCoreRunnable(const CoreRunnable&)=0;
   virtual void NewEventReceiver(const CoreContext&, const EventReceiver&)=0;
-  virtual void NewBolt(const CoreContext&, const std::type_index&, const BoltBase&)=0;
+
+  /// <summary>
+  /// Fired just before a new bolt is inserted in the specified context
+  /// </summary>
+  /// <param name="owningContext">The context which now owns the bolt</param>
+  /// <param name="bolt">The bolt which is about to be added</param>
+  virtual void NewBolt(const CoreContext& owningContext, const std::shared_ptr<BoltBase>& bolt) = 0;
+
   virtual void NewExceptionFilter(const CoreContext&, const ExceptionFilter&)=0;
 };
