086882be211df6d04a1a4e51ba8d0e0d9b31ba70
Graham Tremper <gtremper@gmail.com>
2014-04-10 10:59:29 -0700

    Added additional views to visualizer app

---
+++ AutonetVisualizer/app/scripts/controllers/filtergraph.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('autoNetApp')
+  .controller('FiltergraphCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
