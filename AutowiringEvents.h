ea18cc78e4f0dcfa85faa4fba161259d74e268af
Graham Tremper <gtremper@gmail.com>
2014-04-15 16:34:53 -0700

    Changed layout of visualizer to focus on objects

--- AutowiringEvents.h
+++ AutowiringEvents.h
@@ -2,13 +2,9 @@
 #include "EventReceiver.h"
 #include <list>
 
+class Object;
 class CoreContext;
-class CoreRunnable;
-class ContextMember;
-class EventReceiver;
 class BoltBase;
-class ExceptionFilter;
-
 
 class AutowiringEvents:
   public virtual EventReceiver
@@ -16,18 +12,19 @@ class AutowiringEvents:
 public:
   virtual void NewContext(const CoreContext&)=0;
   virtual void ExpiredContext(const CoreContext&)=0;
-  virtual void NewContextMember(const ContextMember&)=0;
-  virtual void NewCoreRunnable(const CoreRunnable&)=0;
-  virtual void NewEventReceiver(const CoreContext&, const EventReceiver&)=0;
+  virtual void NewObject(const CoreContext&, const Object&)=0;
+  //virtual void NewContextMember(const ContextMember&)=0;
+  //virtual void NewCoreRunnable(const CoreRunnable&)=0;
+  //virtual void NewEventReceiver(const CoreContext&, const EventReceiver&)=0;
 
   /// <summary>
   /// Fired just before a new bolt is inserted in the specified context
   /// </summary>
   /// <param name="owningContext">The context which now owns the bolt</param>
   /// <param name="bolt">The bolt which is about to be added</param>
-  virtual void NewBolt(const CoreContext& owningContext,const std::type_index& sigil, const BoltBase& bolt) = 0;
+  //virtual void NewBolt(const CoreContext& owningContext,const std::type_index& sigil, const BoltBase& bolt) = 0;
 
-  virtual void NewExceptionFilter(const CoreContext&, const ExceptionFilter&)=0;
+  //virtual void NewExceptionFilter(const CoreContext&, const ExceptionFilter&)=0;
   virtual void EventFired(const CoreContext&, const std::type_info&)=0;
 };
 
