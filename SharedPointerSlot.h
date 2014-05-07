b028e7e0244279852af91cfd5bf36ac32b2e183f
Jason Lokerson <cmercenary@gmail.com>
2014-05-06 22:58:06 -0700

    Fixing incorrect static assertion

--- SharedPointerSlot.h
+++ SharedPointerSlot.h
@@ -178,7 +178,7 @@ struct SharedPointerSlotT:
 {
   SharedPointerSlotT(const std::shared_ptr<T>& rhs) {
     static_assert(
-      sizeof(SharedPointerSlotT<T>) == sizeof(SharedPointerSlot),
+      sizeof(std::shared_ptr<T>) == sizeof(m_space),
       "Slot instance is too large to fit in the base type"
     );
 
