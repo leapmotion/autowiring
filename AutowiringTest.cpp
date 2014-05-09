c71b0cfc790f57add9a1a6dbbf5739309507ef9c
Jason Lokerson <cmercenary@gmail.com>
2014-05-09 01:46:08 -0700

    Adding autowiring countercases in order to improve optimization safety

---
+++ AutowiringTest.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "AutowiringTest.h"
+#include "Autowired.h"
+#include "TestFixtures/SimpleObject.h"
+#include "TestFixtures/SimpleReceiver.h"
+
+TEST_F(AutowiringTest, VerifyAutowiredFast) {
+  // Add an object:
+  m_create->Inject<SimpleObject>();
+
+  // Verify that AutowiredFast can find this object
+  AutowiredFast<SimpleObject> sobj;
+  ASSERT_TRUE(sobj.IsAutowired()) << "Failed to autowire an object which was just injected into a context";
+}
+
+TEST_F(AutowiringTest, VerifyAutowiredFastNontrivial) {
+  // This will cause a cache entry to be inserted into the CoreContext's memoization system.
+  // If there is any improper or incorrect invalidation in that system, then the null entry
+  // will create problems when we attempt to perform an AutowiredFast later on.
+  AutowiredFast<CallableInterface>();
+
+  // Now we add the object
+  AutoRequired<SimpleReceiver>();
+
+  // Verify that AutowiredFast can find this object from its interface
+  AutowiredFast<CallableInterface> ci;
+  ASSERT_TRUE(ci.IsAutowired()) << "Failed to autowire an interface advertised by a newly-inserted object";
+}
