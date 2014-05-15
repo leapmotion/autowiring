e566263dc0f430e0512623b271b12d5522968041
codemercenary <jsanchez@leapmotion.com>
2014-05-14 21:30:09 -0700

    Adding a ContextEnumerator and corresponding tests
    This type is an alternative to CoreContext::EnumerateChildContexts.  The EnumerateChildContexts method will eventually be made obsolete in favor of this class, which has much lighter header load and better semantics.

---
+++ ContextEnumeratorTest.cpp
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "ContextEnumeratorTest.h"
+#include "ContextEnumerator.h"
+
+TEST_F(ContextEnumeratorTest, VerifySimpleEnumeration) {
+  // Create a pair of descendant contexts, verify we don't accidentally hit these when enumerating children
+  AutoCreateContext outer;
+  AutoCreateContext inner;
+  CurrentContextPusher pshr(inner);
+
+  // Add a few children to the current context:
+  AutoCreateContext c1;
+  AutoCreateContext c2;
+  AutoCreateContext c3;
+  AutoCreateContext c4;
+
+  std::unordered_set<std::shared_ptr<CoreContext>> allCtxts;
+  allCtxts.insert(AutoCurrentContext());
+  allCtxts.insert(c1);
+  allCtxts.insert(c2);
+  allCtxts.insert(c3);
+  allCtxts.insert(c4);
+
+  // Create the enumerator, verify we get the expected count:
+  size_t nContexts = 0;
+  for(const std::shared_ptr<CoreContext>& cur : ContextEnumerator()) {
+    ASSERT_TRUE(!!allCtxts.count(cur)) << "Context enumerator accidentally enumerated a context not rooted in the specified parent";
+
+    allCtxts.erase(cur);
+    nContexts++;
+  }
+
+  ASSERT_EQ(5UL, nContexts) << "Context enumerator did not encounter the number of expected children";
+  ASSERT_TRUE(allCtxts.empty()) << "Context enumerator did not encounter all children as expected";
+}
