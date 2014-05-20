d11774e3b1e8b61b0bbb5f27f8d673822db6009c
Jason Lokerson <cmercenary@gmail.com>
2014-05-20 16:23:58 -0700

    Moving JunctionBoxBase into its own source file

--- JunctionBox.h
+++ JunctionBox.h
@@ -6,6 +6,7 @@
 #include "EventInputStream.h"
 #include "fast_pointer_cast.h"
 #include "InvokeRelay.h"
+#include "JunctionBoxBase.h"
 #include "JunctionBoxEntry.h"
 #include <boost/thread/mutex.hpp>
 #include <set>
@@ -26,71 +27,6 @@ struct JunctionBoxEntry;
 /// Utility routine which shuts down the current context
 /// </summary>
 void ShutdownCurrentContext(void);
-
-/// <summary>
-/// Used to identify event managers
-/// </summary>
-class JunctionBoxBase {
-public:
-  JunctionBoxBase(void):
-    m_isInitiated(false)
-  {}
-  
-  virtual ~JunctionBoxBase(void);
-
-protected:
-  // Dispatch queue lock:
-  mutable boost::mutex m_lock;
-
-  // Just the DispatchQueue listeners:
-  typedef std::unordered_set<DispatchQueue*> t_stType;
-  t_stType m_dispatch;
-  
-  // This JunctionBox can fire and receive events
-  bool m_isInitiated;
-
-  /// <summary>
-  /// Invokes SignalTerminate on each context in the specified vector.  Does not wait.
-  /// </summary>
-  static void TerminateAll(const std::vector<std::weak_ptr<CoreContext>>& teardown);
-
-  /// <summary>
-  /// Convenience routine for Fire calls
-  /// </summary>
-  /// <remarks>
-  /// This is a convenience routine, its only purpose is to add the "this" parameter to the
-  /// call to FilterFiringException
-  /// </remarks>
-  void FilterFiringException(const std::shared_ptr<EventReceiver>& pReceiver) const;
-
-  /// <summary>
-  /// Converts a dumb pointer into a weak pointer
-  /// </summary>
-  /// <remarks>
-  /// An exterior hold guarantee must be made to call this method safely
-  /// </remarks>
-  static std::weak_ptr<CoreContext> ContextDumbToWeak(CoreContext* pContext);
-
-public:
-  bool IsInitiated(void) const {return m_isInitiated;}
-  void Initiate(void) {m_isInitiated=true;}
-  
-  // Accessor methods:
-  std::vector<std::weak_ptr<EventOutputStreamBase> > * m_PotentialMarshals;
-
-  void SetPotentialMarshals(std::vector<std::weak_ptr<EventOutputStreamBase>> * inVec){
-    m_PotentialMarshals = inVec;
-  }
-
-  const std::unordered_set<DispatchQueue*> GetDispatchQueue(void) const { return m_dispatch; }
-  boost::mutex& GetDispatchQueueLock(void) const { return m_lock; }
-
-  virtual bool HasListeners(void) const = 0;
-
-  // Event attachment and detachment pure virtuals
-  virtual void Add(const JunctionBoxEntry<EventReceiver>& rhs) = 0;
-  virtual void Remove(const JunctionBoxEntry<EventReceiver>& rhs) = 0;
-};
 
 struct NoType {};
 
