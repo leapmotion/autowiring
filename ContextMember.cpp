f87054db452d5f5f245cb57f3d9ac99be921367c
Jason Lokerson <cmercenary@gmail.com>
2014-05-20 23:18:40 -0700

    Eliminated weird Configuration back-reference in ContextMember

--- ContextMember.cpp
+++ ContextMember.cpp
@@ -1,16 +1,7 @@
-// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
 #include "stdafx.h"
 #include "ContextMember.h"
 #include "Autowired.h"
 #include "CoreContext.h"
-#include "Configuration/Config.h"
-
-#ifdef USEAUTONET
-#define ENABLE_NET_MON 1
-#else
-#define ENABLE_NET_MON 0
-#endif
-
 
 ContextMember::ContextMember(const char* name):
   m_name(name),
