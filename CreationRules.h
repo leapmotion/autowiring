404d2d278912de36eb6baca09761c7008d16ead7
Jason Lokerson <cmercenary@gmail.com>
2014-05-21 04:11:48 -0700

    Slight simplification of CreationRules

--- CreationRules.h
+++ CreationRules.h
@@ -45,21 +45,13 @@ struct CreationRules {
     return U::New();
   }
 
-  template<typename U>
-  static typename std::enable_if<!has_static_new<U>::value, U*>::type New(void) {
-    static_assert(!std::is_abstract<U>::value, "Cannot create a type which is abstract");
-    static_assert(has_simple_constructor<U>::value, "Attempted to create a type which did not provide a zero-arguments ctor");
-    return new U;
-  }
-  
-  //
   template<typename U, typename... Args>
   static typename std::enable_if<!has_static_new<U>::value, U*>::type New(Args&&... args) {
     static_assert(!std::is_abstract<U>::value, "Cannot create a type which is abstract");
-    static_assert(sizeof...(Args) != 0, "Template specialization failed");
     static_assert(!has_static_new<U>::value, "Can't inject member with arguments if it has a static new");
-    static_assert(!has_simple_constructor<U>::value, "Can't inject member with arguments if it has a default constructor");
-    
+    static_assert(!sizeof...(Args) || !has_simple_constructor<U>::value, "Can't inject member with arguments if it has a default constructor");
+
+
     return new U(std::forward<Args>(args)...);
   }
 };
