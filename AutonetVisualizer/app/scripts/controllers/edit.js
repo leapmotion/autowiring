5ee0d67e86b3ec8361b21d192cf042cd7127611c
Graham Tremper <gtremper@leapmotion.com>
2014-05-21 18:02:31 -0700

    Can send types from client to server

--- AutonetVisualizer/app/scripts/controllers/edit.js
+++ AutonetVisualizer/app/scripts/controllers/edit.js
@@ -3,9 +3,18 @@
 angular.module('autoNetApp')
 .controller('EditCtrl', ['$scope', 'LeapState', 'websocket', function($scope, LeapState, websocket) {
   $scope.leap = LeapState;
+  $scope.selectedType = LeapState.GetAllTypes()[0];
 
   $scope.terminateContext = function(ctxt){
     console.log("Terminate context: ",ctxt.name);
     websocket.SendMessage("terminateContext", ctxt.id);
   };
+
+  $scope.injectContextMember = function(){
+    // Invalid member
+    if (_.isUndefined($scope.selectedType)) return;
+
+    console.log($scope.selectedType);
+    websocket.SendMessage('injectContextMember',2, $scope.selectedType);
+  }
 }]);
