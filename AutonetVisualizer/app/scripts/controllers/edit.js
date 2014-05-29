4eda6f0edceb32d151d0acc03bec183a26af8531
Graham Tremper <gtremper@gmail.com>
2014-05-29 14:32:59 -0700

    Added warning and success messages to visualizer

--- AutonetVisualizer/app/scripts/controllers/edit.js
+++ AutonetVisualizer/app/scripts/controllers/edit.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('autoNetApp')
-.controller('EditCtrl', ['$scope', 'LeapState', 'websocket', function($scope, LeapState, websocket) {
+.controller('EditCtrl', ['$scope', '$timeout', 'LeapState', 'websocket', function($scope, $timeout, LeapState, websocket) {
   $scope.leap = LeapState;
   $scope.selectedType = undefined;
   $scope.selectedCtxt = undefined;
+  $scope.contextMemberAlerts = [];
 
   $scope.terminateContext = function(ctxt){
     console.log("Terminate context: ",ctxt.name);
@@ -13,9 +14,26 @@ angular.module('autoNetApp')
 
   $scope.injectContextMember = function(){
     // Invalid member
-    if (_.isUndefined($scope.selectedType)) return;
-    if (_.isUndefined($scope.selectedCtxt)) return;
+    if (_.isUndefined($scope.selectedType)) {
+      addContextMemberAlert("<strong>Warning:</strong> Must select a type", "warning");
+      return;
+    }
+    if (_.isUndefined($scope.selectedCtxt)){
+      addContextMemberAlert("<strong>Warning:</strong> Must select a context", "warning");
+      return;
+    }
 
     websocket.SendMessage('injectContextMember', $scope.selectedCtxt, $scope.selectedType);
   }
+
+  function addContextMemberAlert(msg, type) {
+    $scope.contextMemberAlerts.unshift({message:msg, type:type});
+    $timeout(function(){
+      $scope.contextMemberAlerts.pop();
+    },4000);
+  }
+
+  websocket.on('newObject', function(contextID, types, objData) {
+    addContextMemberAlert('<strong>New Object!</strong> ' + objData.name + ' was successfully injected', 'success');
+  });
 }]);
