b478fc6e31932bd56905bc9f06c37fad6e1f0e60
codemercenary <jsanchez@leapmotion.com>
2014-06-02 00:22:19 -0700

    Slot information is now directly a type property and must be queried statically

--- SlotInformation.cpp
+++ SlotInformation.cpp
@@ -48,7 +48,6 @@ SlotInformationStackLocation::~SlotInformationStackLocation(void) {
 }
 
 SlotInformationStackLocation* SlotInformationStackLocation::CurrentStackLocation(void) {
-  // Trivial null defaulting:
   return tss.get();
 }
 
