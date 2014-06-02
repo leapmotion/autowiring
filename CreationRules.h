ecb585b2d4d9e216c1d1908d4affcfb755dac847
Jason Lokerson <cmercenary@gmail.com>
2014-06-01 23:31:29 -0700

    Slot information is now returned directly from the CreationRules

--- CreationRules.h
+++ CreationRules.h
@@ -19,12 +19,19 @@ struct is_injectable
 /// </summary>
 struct CreationRules {
   template<typename U>
-  static typename std::enable_if<has_static_new<U>::value, U*>::type New(void) {
-    return U::New();
+  static typename std::enable_if<
+    has_static_new<U>::value,
+    std::pair<SlotInformationStackLocation, U*>
+  >::type New(void) {
+    return
+      std::make_pair(SlotInformationStackLocation(), U::New());
   }
 
   template<typename U, typename... Args>
-  static typename std::enable_if<!has_static_new<U>::value, U*>::type New(Args&&... args) {
+  static typename std::enable_if<
+    !has_static_new<U>::value,
+    std::pair<SlotInformationStackLocation, U*>
+  >::type New(Args&&... args) {
     static_assert(!std::is_abstract<U>::value, "Cannot create a type which is abstract");
     static_assert(!has_static_new<U>::value, "Can't inject member with arguments if it has a static new");
     static_assert(!sizeof...(Args) || !has_simple_constructor<U>::value, "Can't inject member with arguments if it has a default constructor");
@@ -35,8 +42,10 @@ struct CreationRules {
     try {
       // Stack location and placement new in one expression
       return
-        SlotInformationStackLocation::PushStackLocation<U>(reinterpret_cast<U*>(pSpace)),
-        ::new (pSpace) U(std::forward<Args>(args)...);
+        std::make_pair(
+          SlotInformationStackLocation::PushStackLocation<U>(reinterpret_cast<U*>(pSpace)),
+          ::new (pSpace) U(std::forward<Args>(args)...)
+        );
     }
     catch(...) {
       // Don't want memory leaks--but we also want to avoid calling the destructor, here, so we cast to void before freeing
