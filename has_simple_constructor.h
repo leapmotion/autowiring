82b46fae76cf2f9242ef02b70684ff4e9eff90b6
codemercenary <jsanchez@leapmotion.com>
2014-06-02 13:32:39 -0700

    Splitting off some type traits classes into their own headers

---
+++ has_simple_constructor.h
@@ -0,0 +1,19 @@
+#pragma once
+
+template<typename T, bool isAbstract = std::is_abstract<T>::value>
+struct has_simple_constructor
+{
+  template<class U>
+  static int select(decltype(U())*);
+
+  template<class U>
+  static char select(...);
+
+  static const bool value = sizeof(select<T>(nullptr)) == sizeof(int);
+};
+
+template<typename T>
+struct has_simple_constructor<T, true>
+{
+  static const bool value = false;
+};
