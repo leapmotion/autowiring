6c569edbfbbcd1bc73125165ddc20627f77184bd
Jason Lokerson <cmercenary@gmail.com>
2014-06-06 21:41:32 -0700

    Eliminating use of rethrow_exception and current_exception

--- ExceptionFilter.h
+++ ExceptionFilter.h
@@ -10,15 +10,15 @@ class EventReceiver;
 /// Implements an exception filter type, invoked when an unhandled exception is thrown
 /// </summary>
 /// <remarks>
-/// An exception filter receives an instance of type exception_ptr, and is invoked in
-/// a catch handler from the appropriate site.
+/// An exception filter is called with an exception current in the application, and it
+/// can access this exception either with current_exception or via throw.
 ///
-/// Implementors of this interface must use std::rethrow_exception in order to filter
-/// the passed exception.  Generally, the filtration technique should be written as
-/// follows:
+/// Implementors of this interface must use throw or std::rethrow_exception in order to
+/// filter the passed exception.  Generally, the filtration technique should be written
+/// as follows:
 ///
 /// try {
-///   rethrower(except);
+///   throw;
 /// } catch(custom_type_1& t1) {
 ///   ...handling code...
 /// } catch(custom_type_2& t2) {
@@ -26,16 +26,20 @@ class EventReceiver;
 /// }
 ///
 /// Alternatively, this strategy may be used:
-
+///
 /// try {
-///   rethrower(except);
+///   throw;
 /// } catch(custom_type_1&) {
 /// } catch(custom_type_2&) {
 /// }
 /// ...handling code...
 ///
 /// Filtration methods may safely allow any unhandled rethrows to percolate to the caller.
-/// Unhandled exceptions thrown in a context will cause that context to be torn down.
+///
+/// Unhandled exceptions thrown in a context will cause that context to be torn down.  By
+/// the time the exception filter is called, teardown of the context originating the
+/// exception is inevitable.  The originating context may potentially be torn down already
+/// by the time the filter method is called.
 ///
 /// All filter methods are guaranteed to be called in the thread and context of the code
 /// which threw the exception.  In the case of Fire, it will be in the context of the
@@ -49,26 +53,22 @@ public:
   /// <summary>
   /// This method is invoked when an exception has been thrown by CoreThread::Run
   /// </summary>
-  /// <param name="rethrower">A function which, when called, will throw the exception being filtered</param>
   /// <remarks>
   /// Such an exception is treated as a fatal exception, and will unconditionally result
   /// in the enclosing context being torn down.  Filters MUST NOT attempt to alter this
   /// behavior.  If this behavior is not desirable, greater care should be taken to
   /// prevent certain exceptions from being unhandled.
   ///
-  /// The rethrower lambda must be called before this filter method exits.  Attempting to cache this function
-  /// will result in undefined behavior.
+  /// Implementors can use "throw" with no arguments to trigger a rethrow of the originating exception.
   /// </remarks>
-  virtual void Filter(const std::function<void()>& rethrower) {};
+  virtual void Filter(void) {};
 
   /// <summary>
   /// This is an exception filter for exceptions thrown during a call to Fire
   /// </summary>
-  /// <param name="rethrower">A function which, when called, will throw the exception being filtered</param>
   /// <param name="pRecipient">The target of the call</param>
   /// <remarks>
-  /// The rethrower lambda must be called before this filter method exits.  Attempting to cache this function
-  /// will result in undefined behavior.
+  /// Implementors can use "throw" with no arguments to trigger a rethrow of the originating exception.
   /// </remarks>
-  virtual void Filter(const std::function<void()>& rethrower, const JunctionBoxBase* pJunctionBox, EventReceiver* pRecipient) {}
+  virtual void Filter(const JunctionBoxBase* pJunctionBox, EventReceiver* pRecipient) {}
 };
