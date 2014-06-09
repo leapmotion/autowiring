f34bbfe1bdf091d6910e67a29a56797284d22c15
Jason Lokerson <cmercenary@gmail.com>
2014-06-09 15:58:45 -0700

    Adding the ability to pass arguments to factory New

--- has_static_new.h
+++ has_static_new.h
@@ -1,13 +1,13 @@
 #pragma once
 
-template<typename T>
+template<typename T, typename... Args>
 struct has_static_new
 {
   template<class Fn, Fn>
   struct unnamed_constant;
 
   template<class U>
-  static int select(unnamed_constant<U* (*)(), &U::New>*);
+  static int select(decltype(U::New(*(Args*)nullptr...))*);
 
   template<class U>
   static char select(...);
