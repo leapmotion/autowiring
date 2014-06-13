3e1d26306db58f576d34e413d7038a16f838d94f
Jason Lokerson <cmercenary@gmail.com>
2014-06-13 03:57:19 -0700

    Adding a utility AutoConstruct type to function similarly to AutoRequired

--- Autowired.h
+++ Autowired.h
@@ -272,6 +272,19 @@ public:
   bool IsAutowired(void) const {return std::shared_ptr<T>::get() != nullptr;}
 };
 
+/// <summary>
+/// Convenience class functionally identical to AutoRequired, but specialized to forward ctor arguments
+/// </summary>
+template<class T>
+class AutoConstruct:
+  public AutoRequired<T>
+{
+public:
+  template<class... Args>
+  AutoConstruct(Args&&... args) :
+    AutoRequired(CoreContext::CurrentContext(), std::forward<Args>(args)...)
+  {}
+};
 
 /// <summary>
 /// Convenience class to create an event firer. Also caches the associated JunctionBox
