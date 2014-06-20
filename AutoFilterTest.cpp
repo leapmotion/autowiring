d62ff6b2f5f8ac8fc3d0423ec32f32219c6bad4a
James Donald <jdonald@leapmotion.com>
2014-06-20 11:50:33 -0700

    Disable DeferredRecieptInSubContext (sic) until made reliable on Windows, #9802

--- AutoFilterTest.cpp
+++ AutoFilterTest.cpp
@@ -420,7 +420,12 @@ public:
   size_t nReceived;
 };
 
-TEST_F(AutoFilterTest, DeferredRecieptInSubContext) {
+#if _WIN32
+TEST_F(AutoFilterTest, DISABLED_DeferredRecieptInSubContext)
+#else
+TEST_F(AutoFilterTest, DeferredRecieptInSubContext)
+#endif
+{
   AutoCurrentContext()->Initiate();
 
   static const size_t nPackets = 5;
