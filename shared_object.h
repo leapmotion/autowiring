84ca7613758cded12fba7c64d7b6e3f0adbe68e0
Gabriel Hare <Gabriel.Archacki.Hare@gmail.com>
2014-06-27 01:05:08 -0700

    Redesign of shareable objects.
    atomic_object handles synchronization via atomic methods, but is not a shared resource.
    shared_object guarantees existence and implements atomic access to a shared mutex_object.
    unlock_object guarantees both existence and mutability of an shared_object. Because unlock_object  has no copy methods it encourages correct scoped usage.

--- shared_object.h
+++ shared_object.h
@@ -4,71 +4,58 @@
 #include "stdafx.h"
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/lock_guard.hpp>
-#include "guard_object.h"
-
-template<class object> class guard_object;
+#include "atomic_object.h"
 
 ///<summary>
-///This class provides a standard thread-safe wrapper for data structures.
-///Because instantiation may be separate from initialization, this class
-///also tracks the initialization status.
+///This class implements the atomic_object interface to a shared reference.
+///This will enable the construction of unlock_objects that combine the existential
+///guarantee of a shared_ptr with the immutability guarantee of a lock_guard.
 ///</summary>
 ///<remarks>
 ///This class supports two modes of use:
 /// - Atomic assignments which can be used to create a consistent local copy,
 ///   or to modify the contents of shared_object.
-/// - Held locks, via guard_object, which will block all other interactions.
+/// - Held locks, via unlock_object, which will block all other interactions.
 ///</remarks>
 template<class object>
 class shared_object {
-  friend class guard_object<object>;
+  friend class unlock_object<object>;
 
-  //CONTRACT: If !m_initialized then m_object == object();
-  mutable boost::mutex m_lock;
-  bool m_initialized;
-  object m_object;
+  //CONTRACT: m_share == true
+  std::shared_ptr<atomic_object<object>> m_share;
 
 public:
   ///<summary>
-  ///Default constructor yielding initialized() == false.
+  ///Default constructor instiates an uninitialized atomic_object.
   ///</summary>
   shared_object():
-  m_initialized(false) {};
+  m_share(new atomic_object<object>()) {}
 
   ///<summary>
-  ///Copy constructor yielding initialized() == _arg.initialized().
+  ///Copy constructor instantiates a atomic_object initialized by _arg.
   ///</summary>
-  shared_object(shared_object& _arg) {
-    boost::lock_guard<boost::mutex> lock_arg(_arg.m_lock);
-    m_initialized = _arg.m_initialized;
-    m_object = _arg.m_object;
-  }
+  shared_object(const shared_object& _arg):
+  m_share(_arg.m_share) {}
 
   ///<summary>
-  ///Assignment yielding initialized() == _rhs.initialized().
+  ///Copy constructor instiates a atomic_object initialized by _arg.
   ///</summary>
-  shared_object& operator = (const shared_object& _rhs) {
-    boost::lock_guard<boost::mutex> lock_this(m_lock);
-    boost::lock_guard<boost::mutex> lock_rhs(_rhs.m_lock);
-    m_initialized = _rhs.m_initialized;
-    m_object = _rhs.m_object;
+  shared_object(const object& _arg):
+  m_share(new atomic_object<object>(_arg)) {}
+
+  ///<summary>
+  ///Assignment to current atomic_object.
+  ///</summary>
+  shared_object& operator = (const shared_object& _arg) {
+    m_share = _arg.m_share;
     return *this;
   }
 
   ///<summary>
-  ///Initialization yielding initialized() == true.
+  ///Assignment to current atomic_object.
   ///</summary>
-  shared_object(const object& _arg):
-  m_initialized(true),
-  m_object(_arg) {}
-
-  ///<summary>
-  ///Assignment yielding initialized() == true.
-  ///</summary>
-  shared_object& operator = (const object& _rhs) {
-    boost::lock_guard<boost::mutex> lock_this(m_lock);
-    m_initialized = true;
-    m_object = _rhs;
+  shared_object& operator = (const object& _arg) {
+    *m_share = _arg;
     return *this;
   }
 
@@ -76,14 +63,12 @@ public:
   ///Cast by copying current state
   ///</summary>
   operator object() const {
-    boost::lock_guard<boost::mutex> lock_this(m_lock);
-    return m_object;
+    return static_cast<object>(*m_share);
   }
 
   ///<return>True if the object was not assigned default values</return>
   bool initialized() const {
-    boost::lock_guard<boost::mutex> lock_this(m_lock);
-    return m_initialized;
+    return m_share->initialized();
   }
 
   ///<summary>
@@ -91,36 +76,13 @@ public:
   ///</summary>
   ///<return>True if the object was not assigned default values</return>
   bool initialized(object& _use) {
-    boost::lock_guard<boost::mutex> lock_this(m_lock);
-    if (m_initialized)
-      _use = m_object;
-    return m_initialized;
+    return m_share->initialized(_use);
   }
 
   ///<summary>
   ///Reset using default constructor yielding initialized() == false.
   ///</summary>
   void reset() {
-    boost::lock_guard<boost::mutex> lock_this(m_lock);
-    m_initialized = false;
-    m_object = object();
-  }
-
-  ///<summary>
-  ///Creates a shared reference to this object that also maintains a lock
-  //</summary>
-  guard_object<object> hold() {
-    return guard_object<object>(*this);
-  }
-
-  ///<summary>
-  ///Tries to create a shared reference to this object that also maintains a lock,
-  ///or returns an empty guard object.
-  ///</summary>
-  guard_object<object> try_hold() {
-    if (m_lock.try_lock()) {
-      return guard_object<object>(*this, boost::adopt_lock);
-    }
-    return guard_object<object>();
+    m_share->reset();
   }
 };
