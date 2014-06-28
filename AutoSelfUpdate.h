13883e0279f09d5c13a0097cd0e98f461538b910
Gabriel Hare <ghare@leapmotion.com>
2014-06-27 18:02:29 -0700

    Adding a standard self-update wrapping, incorporating the atomic_object locking.

--- AutoSelfUpdate.h
+++ AutoSelfUpdate.h
@@ -1,14 +1,26 @@
-//
-//  AutoSelfUpdate.h
-//  Platform
-//
-//  Created by Gabriel Hare on 6/27/14.
-//
-//
+#pragma once
+#include "Autowiring/NewAutoFilter.h"
+#include "Autowiring/atomic_object.h"
+#include "Autowiring/unlock_object.h"
 
-#ifndef Platform_AutoSelfUpdate_h
-#define Platform_AutoSelfUpdate_h
+///<summary>
+///Enables an automatic self-update when a packet is decorated with the object type.
+///</summary>
+///<remarks>
+///In order to ensure that this method will be consistent with any other AutoFilter calls,
+///the object inherits from atomic_object, which implements basic locking functionality.
+///</remarks>
+template<class object, class lock = boost::mutex>
+class AutoSelfUpdate:
+public atomic_object<object, lock> {
+public:
 
+  using atomic_object<object, lock>::operator =;
+  using atomic_object<object, lock>::operator object;
 
+  void SelfUpdate(object& update) {
+    atomic_object<object, lock>::operator = (update);
+  }
 
-#endif
+  NewAutoFilter<decltype(&AutoSelfUpdate<object>::SelfUpdate), &AutoSelfUpdate<object>::SelfUpdate> AutoFilter_SelfUpdate;
+};
