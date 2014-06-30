3326bd1023d8cc2990f946d38b04c969732da4cf
Graham Tremper <gtremper@leapmotion.com>
2014-06-30 11:37:39 -0700

    Added 'dist' to autonet repository so we can deploy to heroku

--- AutonetVisualizer/web.js
+++ AutonetVisualizer/web.js
@@ -1,7 +1,10 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000;
  
 //app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+
+app.listen(port);
+console.log("Listening on port: " + port);
