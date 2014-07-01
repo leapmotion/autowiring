a1872c009c53cef0aafd1f51325b2b7430e00254
Graham Tremper <gtremper@leapmotion.com>
2014-06-30 17:07:01 -0700

    AutoNetServer now self contained
    It no longer inherits from WebSocketServerBase

--- AutonetVisualizer/deploy.sh
+++ AutonetVisualizer/deploy.sh
@@ -7,6 +7,9 @@
 #This script will the autonet client on the current branch
 #to the heroku host
 
+# Be sure to run 'grunt build' before and commit any changes
+# to the 'dist' directory that result
+
 
 # Go to root git directory
 cd "$(git rev-parse --show-toplevel)"
@@ -15,4 +18,5 @@ cd "$(git rev-parse --show-toplevel)"
 branch=`git rev-parse --abbrev-ref HEAD`
 echo "Deploying branch '$branch' to heroku"
 
+# Push AutonetVisualizer as subtree to heroku
 git push heroku `git subtree split --prefix="source/Autowiring/AutonetVisualizer" $branch`:master
