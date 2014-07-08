e3bea61cde12d1974270980a67da89d85ac1b06a
Gabriel Hare <ghare@leapmotion.com>
2014-07-08 13:08:09 -0700

    Changed AutoSelfUpdate to inherit from atomic_object. Changed HandRegionTransport to be a shared_object, consistent with current use.

--- AutoSelfUpdate.h
+++ AutoSelfUpdate.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "Autowiring/NewAutoFilter.h"
-#include "Autowiring/shared_object.h"
-#include "Autowiring/unlock_object.h"
+#include "Autowiring/atomic_object.h"
 
 ///<summary>
 ///Enables an automatic self-update when a packet is decorated with the object type.
+///Provides the prior object (the last decorated instance) to all subsequent packets.
 ///</summary>
 ///<remarks>
 ///In order to ensure that this method will be consistent with any other AutoFilter calls,
@@ -12,14 +12,15 @@
 ///</remarks>
 template<class object, class lock = boost::mutex>
 class AutoSelfUpdate:
-public shared_object<object, lock> {
+public atomic_object<object, lock> {
 public:
 
-  using shared_object<object, lock>::operator =;
-  using shared_object<object, lock>::operator object;
+  void AutoFilter(AutoPacket& packet) {
+    //TODO: Decorate with prior<object>
+  }
 
   void SelfUpdate(object& update) {
-    shared_object<object, lock>::operator = (update);
+    atomic_object<object, lock>::operator = (update);
   }
 
   NewAutoFilter<decltype(&AutoSelfUpdate<object>::SelfUpdate), &AutoSelfUpdate<object>::SelfUpdate> AutoFilter_SelfUpdate;
