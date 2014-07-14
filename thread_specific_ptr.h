5d666923f3e1c4b67bcaad47b661152f51a3d93f
Graham Tremper <gtremper@leapmotion.com>
2014-07-14 11:39:19 -0700

    Created basic version of thread_specific_ptr

--- thread_specific_ptr.h
+++ thread_specific_ptr.h
@@ -2,6 +2,14 @@
 #include "C++11/cpp11.h"
 #include FUNCTIONAL_HEADER
 
+#if _WIN32
+#define THREAD_LOCAL __declspec( thread )
+#else
+#define THREAD_LOCAL __thread
+#endif
+
+namespace leap {
+
 // <summary>
 // Holds a ptr in thread local storage. Same interface as boost::thread_specific_ptr
 // </summary>
@@ -9,18 +17,12 @@ template<typename T>
 class thread_specific_ptr {
 public:
   thread_specific_ptr():
-    m_cleanupFunction([this]{delete get();}),
-    m_ptr(nullptr)
-  {
-    m_ptr = new T;
-  }
+    m_cleanupFunction([](T* p){ delete p; })
+  {}
   
   thread_specific_ptr(std::function<void(T*)> cleanup):
-    m_cleanupFunction(cleanup),
-    m_ptr(nullptr)
-  {
-    m_ptr = new T;
-  }
+    m_cleanupFunction(cleanup)
+  {}
   
   virtual ~thread_specific_ptr(){
     reset();
@@ -39,19 +41,31 @@ public:
   }
   
   T* release() {
-    T* temp = get();
+    T* const temp = get();
     m_ptr = nullptr;
     return temp;
   }
   
   void reset(T* new_value=nullptr) {
-    if (m_ptr != new_value && m_ptr != nullptr) {
-      m_cleanupFunction();
+    T* const current_value = get();
+    
+    if (current_value != new_value) {
+      m_cleanupFunction(current_value);
       m_ptr = new_value;
     }
   }
   
 private:
+  // Functions called the cleanup old value. Defaults to "delete m_ptr"
   std::function<void(T*)> m_cleanupFunction;
-  T* m_ptr;
+  
+  // Store our pointer in thread-local-storage
+  THREAD_LOCAL static T* m_ptr;
 };
+
+  // Default value
+template <typename T>
+THREAD_LOCAL T* thread_specific_ptr<T>::m_ptr = nullptr;
+  
+} //namespace leap
+
