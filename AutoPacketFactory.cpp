2ab251db4a89da0b5971080611cd6bf8fc3f6c07
Graham Tremper <gtremper@gmail.com>
2014-07-14 13:50:44 -0700

    Confirmed leap::thread_specific_ptr still working

--- AutoPacketFactory.cpp
+++ AutoPacketFactory.cpp
@@ -2,10 +2,8 @@
 #include "AutoPacketFactory.h"
 #include "AutoPacket.h"
 #include "thread_specific_ptr.h"
-#include <boost/thread/tss.hpp>
 
-//leap::thread_specific_ptr<NewAutoFilterBase*> pAFB;
-static boost::thread_specific_ptr<NewAutoFilterBase*> pAFB;
+static leap::thread_specific_ptr<NewAutoFilterBase*> pAFB;
 
 AutoPacketFactory::AutoPacketFactory(void):
   ContextMember("AutoPacketFactory"),
--- SlotInformation.cpp
+++ SlotInformation.cpp
@@ -3,12 +3,10 @@
 #include "InterlockedExchange.h"
 #include "Autowired.h"
 #include "thread_specific_ptr.h"
-#include <boost/thread/tss.hpp>
 #include MEMORY_HEADER
 
 // Special file-level allocation with a no-op dtor, because all stack locations are stack-allocated
-//leap::thread_specific_ptr<SlotInformationStackLocation> tss([](SlotInformationStackLocation*) {});
-static boost::thread_specific_ptr<SlotInformationStackLocation> tss([](SlotInformationStackLocation*) {});
+static leap::thread_specific_ptr<SlotInformationStackLocation> tss([](SlotInformationStackLocation*) {});
 
 SlotInformationStackLocation::SlotInformationStackLocation(SlotInformationStumpBase* pStump, const void* pObj, size_t extent) :
   m_pPrior(tss.get()),
