2be91d358eeda4343ed17742c5fd4bb20f3db398
Jason Lokerson <cmercenary@gmail.com>
2014-07-16 18:20:33 -0700

    Splitting CoreThread platform-specific sources for Mac and Linux

--- CoreThreadLinux.cpp
+++ CoreThreadLinux.cpp
@@ -7,11 +7,7 @@
 
 void BasicThread::SetCurrentThreadName(void) const {
   if(IS_INTERNAL_BUILD)
-#if __APPLE__
-    pthread_setname_np(m_name);
-#else
     pthread_setname_np(pthread_self(), m_name);
-#endif
 }
 
 boost::chrono::system_clock::time_point BasicThread::GetCreationTime(void) {
