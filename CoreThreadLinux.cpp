9826decaae8b98780f2f5e59c198abc46f23a3ea
Graham Tremper <gtremper@gmail.com>
2014-07-21 12:19:40 -0700

    Changed windows GetThreadTimes to use milliseconds

--- CoreThreadLinux.cpp
+++ CoreThreadLinux.cpp
@@ -5,6 +5,10 @@
 #include <pthread.h>
 #include <sys/resource.h>
 
+using boost::chrono::seconds;
+using boost::chrono::milliseconds;
+using boost::chrono::microseconds;
+
 void BasicThread::SetCurrentThreadName(void) const {
   if(IS_INTERNAL_BUILD)
     pthread_setname_np(pthread_self(), m_name);
@@ -14,9 +18,9 @@ boost::chrono::system_clock::time_point BasicThread::GetCreationTime(void) {
   return boost::chrono::system_clock::time_point::min();
 }
 
-void BasicThread::GetThreadTimes(boost::chrono::nanoseconds& kernelTime, boost::chrono::nanoseconds& userTime) {
+void BasicThread::GetThreadTimes(boost::chrono::milliseconds& kernelTime, boost::chrono::milliseconds& userTime) {
   rusage usage;
   getrusage(RUSAGE_SELF, &usage);
-  kernelTime = boost::chrono::seconds(usage.ru_stime.tv_sec) + boost::chrono::microseconds(usage.ru_stime.tv_usec);
-  userTime = boost::chrono::seconds(usage.ru_utime.tv_sec) + boost::chrono::microseconds(usage.ru_utime.tv_usec);
+  kernelTime = boost::chrono::duration_cast<milliseconds>(seconds(usage.ru_stime.tv_sec) + microseconds(usage.ru_stime.tv_usec));
+  userTime = boost::chrono::duration_cast<milliseconds>(seconds(usage.ru_utime.tv_sec) + microseconds(usage.ru_utime.tv_usec));
 }
