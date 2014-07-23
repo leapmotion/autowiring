c76ae9e410b6208263ac2afe79344b8cb71b0c85
Jason Lokerson <cmercenary@gmail.com>
2014-07-23 16:44:25 -0700

    Updating standard include headers to reduce code reduplication

--- test/stdafx.h
+++ test/stdafx.h
@@ -1,20 +1,15 @@
 // Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
-#ifndef _STDAFX_H
-#define _STDAFX_H
+#pragma once
 
-#include <math.h>
-#include <assert.h>
 #include "C++11/cpp11.h"
-
-#ifndef _MSC_VER
-  #include <stdlib.h>
-#endif
+#include "src/Autowired.h"
+#include <gtest/gtest.h>
+#include <thread>
+#include <mutex>
 
 // Very unusual syntax -- function taking an array of fixed size, and returning
 // a character array of that same size
 template<class T, int n>
-const char (&ArraySize(const T (&vals)[n]))[n];
+const char(&ArraySize(const T(&vals)[n]))[n];
 
 #define ARRAYCOUNT(x) sizeof(ArraySize(x))
-
-#endif
