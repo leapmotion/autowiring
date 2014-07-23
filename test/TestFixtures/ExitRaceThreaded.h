4779fdec4548ff87480d59df1224a27c24408eb0
Jason Lokerson <cmercenary@gmail.com>
2014-07-23 00:44:25 -0700

    Fixing and rearranging CMakeLists.txt

--- test/TestFixtures/ExitRaceThreaded.h
+++ test/TestFixtures/ExitRaceThreaded.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Autowiring/CoreThread.h"
+#include "CoreThread.h"
 
 /// <summary>
 /// An exit-race signal
---
+++ test/stdafx.cpp
@@ -0,0 +1,3 @@
+// Copyright (c) 2010 - 2013 Leap Motion. All rights reserved. Proprietary and confidential.
+#include "stdafx.h"
+#include "ocuConfig.h"
