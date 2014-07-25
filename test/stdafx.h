4db9a01a6544d7d83959eb30596fbf15824591f4
Jason Lokerson <cmercenary@gmail.com>
2014-07-25 02:01:39 -0700

    Attempting to speed up non-PCH builds

--- test/stdafx.h
+++ test/stdafx.h
@@ -2,10 +2,13 @@
 #pragma once
 
 #include "C++11/cpp11.h"
-#include "src/Autowired.h"
 #include <gtest/gtest.h>
-#include <thread>
-#include <mutex>
+
+#ifdef _MSC_VER
+  #include "src/Autowired.h"
+  #include <thread>
+  #include <mutex>
+#endif
 
 // Very unusual syntax -- function taking an array of fixed size, and returning
 // a character array of that same size
