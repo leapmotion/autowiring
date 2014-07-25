ee8832889b8d49859d190dbb8874c54981cce736
Jason Lokerson <cmercenary@gmail.com>
2014-07-25 02:18:47 -0700

    Eliminating accidentally included CTestTestfile, ignore list updated

--- C++11/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Development/platform/source/C++11
-# Build directory: F:/Development/platform/source/C++11
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
