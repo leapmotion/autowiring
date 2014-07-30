d4a6f45c38a90e5fc963685b51ff3d5fbb031c9e
Jason Lokerson <cmercenary@gmail.com>
2014-07-29 21:25:32 -0700

    enable_if::type cannot form part of a template parameter space

--- contrib/json11/json11.hpp
+++ contrib/json11/json11.hpp
@@ -101,17 +101,27 @@ public:
     Json(const T & t) : Json(t.to_json()) {}
 
     // Implicit constructor: map-like objects (std::map, std::unordered_map, etc)
-    template <class M, typename std::enable_if<
-        std::is_constructible<std::string, decltype(std::declval<M>().begin()->first)>::value
-        && std::is_constructible<Json, decltype(std::declval<M>().begin()->second)>::value,
-            int>::type = 0>
-    Json(const M & m) : Json(object(m.begin(), m.end())) {}
+    template <class M>
+    Json(
+        const typename std::enable_if<
+            std::is_constructible<std::string, decltype(std::declval<M>().begin()->first)>::value &&
+            std::is_constructible<std::string, decltype(std::declval<M>().begin()->second)>::value,
+            M
+        >::type& m
+    ) :
+        Json(object(m.begin(), m.end()))
+    {}
 
     // Implicit constructor: vector-like objects (std::list, std::vector, std::set, etc)
-    template <class V, typename std::enable_if<
-        std::is_constructible<Json, decltype(*std::declval<V>().begin())>::value,
-            int>::type = 0>
-    Json(const V & v) : Json(array(v.begin(), v.end())) {}
+    template <class V>
+    Json(
+        const typename std::enable_if<
+            std::is_constructible<Json, decltype(std::declval<M>().begin()->first)>::value,
+            V
+        >::type& v
+    ) :
+        Json(array(v.begin(), v.end()))
+    {}
 
     // This prevents Json(some_pointer) from accidentally producing a bool. Use
     // Json(bool(some_pointer)) if that behavior is desired.
