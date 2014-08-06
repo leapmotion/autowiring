bb3056ddd6e7eae2ca39e51cebe6f56bc5cee82e
Gabriel Hare <ghare@leapmotion.com>
2014-08-05 17:27:40 -0700

    Enabled post-hoc injection via Boltable declaration.

--- autowiring/MicroBolt.h
+++ autowiring/MicroBolt.h
@@ -1,6 +1,8 @@
 // Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.
 #pragma once
 #include "Bolt.h"
+#include "CoreContext.h"
+#include "ContextEnumerator.h"
 
 /// <summary>
 /// Causes the class inheriting from this definition to be bolted to the specified contexts
@@ -16,10 +18,19 @@ struct MicroBolt:
   public Bolt<Sigils...>
 {
 public:
+  MicroBolt(void) {
+    // ASSERT: Inject<T>() is idempotent.
+    // NOTE: Injection of T into all matching contexts may result in
+    // multiple calls to Inject<T>() if a matching context
+    // is created during traversal.
+    std::shared_ptr<CoreContext> rootContext = CoreContext::CurrentContext();
+    auto listSigils = {ContextEnumeratorT<Sigils>(rootContext)...};
+    for (auto findSigil : listSigils)
+      for (auto context : findSigil)
+        context->template Inject<T>();
+  }
   void ContextCreated(void) override;
 };
-
-#include "CoreContext.h"
 
 template<typename T, typename... Sigils>
 void MicroBolt<T, Sigils...>::ContextCreated(void) {
