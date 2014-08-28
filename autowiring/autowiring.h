3fbedf623b9162c96e0e85b9cf89d0c51714c66a
Graham Tremper <gtremper@leapmotion.com>
2014-08-28 11:34:00 -0700

    Add AutowiringConfig.h to define preprocessor macros from CMake

--- autowiring/autowiring.h
+++ autowiring/autowiring.h
@@ -1,1 +1,9 @@
+#pragma once
+#include "AutowiringConfig.h"
+
 #include "Autowired.h"
+
+// Include AutoNetSever if we're building autonet
+#if AUTOWIRING_BUILD_AUTONET
+#include "AutoNetServer.h"
+#endif
