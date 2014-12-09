a39a55e030f126e862f2caf09b14e6ea182851c1
Jason Lokerson <cmercenary@gmail.com>
2014-12-09 14:29:44 -0800

    Suggested modifications to the CoreRunnable interface

--- autowiring/CoreRunnable.h
+++ autowiring/CoreRunnable.h
@@ -9,15 +9,53 @@ public:
   CoreRunnable(void);
   virtual ~CoreRunnable(void);
   
+private:
+  // Set to true if this runnable was ever signalled to start
+  bool m_wasStarted;
+
+  // The outstanding count, held for as long as processing is underway
+  std::shared_ptr<Object> m_outstanding;
+
+  // Set to true if this runnable should terminate processing
+  bool m_shouldStop;
+
+protected:
+  std::mutex m_lock;
+  std::condition_variable m_cv;
+
+  /// <summary>
+  /// Method invoked from Start when this class is being asked to begin processing
+  /// </summary>
+  /// <returns>True if processing has started, false otherwise</returns>
+  /// <remarks>
+  /// This method will be called at most once.
+  /// </remarks>
+  virtual bool DoStart(void) = 0;
+
+  /// <summary>
+  /// Invoked by the base class when a Stop call has been made
+  /// </summary>
+  /// <remarks>
+  /// This method will be called at most once, and may potentially be called even 
+  /// </remarks>
+  virtual void OnStop(bool graceful) = 0;
+
+public:
+  // Accessor methods:
+  bool IsRunning(void) const { return (bool)m_outstanding; }
+  bool WasStarted(void) const { return m_wasStarted; }
+  bool ShouldStop(void) const { return m_shouldStop; }
+
   /// <summary>
   /// Causes this runnable to begin processing
   /// </summary>
+  /// <returns>True if this call resulted in a successful start the first time, false in all other cases</returns>
   /// <remarks>
   /// It is an error to call this routine more than once.  The passed outstanding shared pointer
   /// is used to keep tracking of number of simultaneous runnables outstanding.  This routine may
   /// be called even after Stop has been called; the caller MUST return false in this case.
   /// </remarks>
-  virtual bool Start(std::shared_ptr<Object> outstanding) = 0;
+  void Start(std::shared_ptr<Object> outstanding);
 
   /// <summary>
   /// Stops this runnable, optionally performing graceful cleanup if requested
@@ -26,20 +64,15 @@ public:
   /// On return of this method, regardless of the return value, a subsequent call to Wait is
   /// guaranteed to either return immediately, or once the thread implementation completes.
   /// </remarks>
-  virtual void Stop(bool graceful) = 0;
-
-  /// <returns>
-  /// True if this runnable is currently running
-  /// </returns>
-  virtual bool IsRunning(void) const = 0;
-
-  /// <returns>
-  /// True if this runnable has been told to stop
-  /// </returns>
-  virtual bool ShouldStop(void) const = 0;
+  virtual void Stop(bool graceful);
 
   /// <summary>
   /// Waits for this object to start running, and then stop running
   /// </summary>
-  virtual void Wait(void) = 0;
+  void Wait(void);
+
+  /// <summary>
+  /// Waits for this object to start running, and then stop running
+  /// </summary>
+  void WaitFor(std::chrono::nanoseconds timeout);
 };
--- src/autowiring/CoreRunnable.cpp
+++ src/autowiring/CoreRunnable.cpp
@@ -2,6 +2,23 @@
 #include "stdafx.h"
 #include "CoreRunnable.h"
 
-CoreRunnable::CoreRunnable(void){}
+CoreRunnable::CoreRunnable(void):
+m_wasStarted(false),
+  m_shouldStop(false)
+{}
 
 CoreRunnable::~CoreRunnable(void){}
+
+void CoreRunnable::Start(std::shared_ptr<Object> outstanding) {
+  if(m_outstanding || m_wasStarted)
+    // We have already been started, end here
+    return;
+
+  m_wasStarted = true;
+  if(!DoStart()) {
+    m_shouldStop = true;
+    return;
+  }
+
+  m_outstanding = outstanding;
+}
