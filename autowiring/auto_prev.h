a9eb9f4b65afc8fd84324facf577801160c2f88f
Graham Tremper <gtremper@gmail.com>
2014-12-19 15:17:09 -0800

    Add auto_prev AutoFilter identifier

---
+++ autowiring/auto_prev.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2012-2014 Leap Motion, Inc. All rights reserved.
+#pragma once
+
+/// <summary>
+/// Identifier for AutoFilter inputs from the previous packet
+/// </summary>
+/// <remarks>
+/// When auto_prev is used as an AutoFilter argument, it's value will the value of 'T'
+/// from the previous packet. It's null for the the first packet
+/// </remarks>
+template<class T>
+struct auto_prev {};
