544804de2ed576e4cd9cc63cf08053980f6b6c52
Jason Lokerson <cmercenary@gmail.com>
2014-12-26 07:58:25 -0800

    Elimination of the now-empty Invalidate function
    This function was once necessary to trigger an object pool flush, but as the object pool concept was removed, there is no longer any need for Invalidate.

--- autowiring/AutoPacketFactory.h
+++ autowiring/AutoPacketFactory.h
@@ -60,9 +60,6 @@ private:
   // Returns the internal outstanding count, for use with AutoPacket
   std::shared_ptr<void> GetInternalOutstanding(void);
 
-  // Recursive invalidation routine, causes AutoPacket object pools to be dumped to the root
-  void Invalidate(void);
-
   // Utility override, does nothing
   void AddSubscriber(std::false_type) {}
 
