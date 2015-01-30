5f6d2570c3b0a4dee91a882ca1d56a95d4077519
Graham Tremper <gtremper@gmail.com>
2015-01-30 15:21:37 -0800

    Add mac auto_future header

--- autowiring/auto_future.h
+++ autowiring/auto_future.h
@@ -3,4 +3,6 @@
 
 #ifdef _MSC_VER
 #include "auto_future_win.h"
+#elif defined(__APPLE__)
+#include "auto_future_mac.h"
 #endif
---
+++ autowiring/auto_future_mac.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <future>
+#include <stdexcept>
+
+namespace autowiring {
+  /// <summary>
+  /// Platform-specific operation appending routine
+  /// </summary>
+  /// <param name="future">The future to be appended to</param>
+  /// <param name="fn">The lambda to be executed after the future is ready</param>
+  template<typename T, typename Fn>
+  void then(std::future<T>& future, Fn&& fn) {
+    
+  }
+}
