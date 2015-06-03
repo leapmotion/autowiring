d873f8e32a2fdb3fa8204db0c6803a2f00d45647
Victor 'Yes, The Victor' Dods <vdods@leapmotion.com>
2015-06-03 14:56:55 -0700

    Added the framework for a series of tutorials on the main Autowiring features.

---
+++ examples/AutoFilterTutorial00.cpp
@@ -0,0 +1,35 @@
+/// @page AutoFilterTutorial00 AutoFilter Tutorial 00
+/// This tutorial is intended to teach blah blah blah.
+/// @code
+
+// AutoFilterTutorial00.cpp created by Victor Dods on 2015/06/03
+// Copyright (C) Leap Motion, Inc. All rights reserved.
+
+/// @endcode
+/// 
+/// Testing
+/// 
+/// # Markdown test heading 1
+/// ## Heading 2
+/// ### Heading 3
+/// 
+/// List:
+/// - item1
+/// - item2
+///   - item2.1
+///   - item2.2
+///   - item2.3
+/// - item3
+/// 
+/// Numbered list:
+/// 1. Blah 1
+/// 2. Blah 2
+/// 3. Blah 3
+/// 
+/// @code
+
+int main () {
+  return 0;
+}
+
+/// @endcode
