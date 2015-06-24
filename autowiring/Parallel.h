49744c03bec3cca6cc0c8fb2755b110c66ad0006
Graham Tremper <gtremper@gmail.com>
2015-06-24 14:55:21 -0700

    Create files for autowiring::parallel feature

---
+++ autowiring/Parallel.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
+#pragma once
+
+namespace autowiring {
+
+class parallel {
+
+};
+
+
+
+
+}//namespace autowiring
---
+++ src/autowiring/test/ParallelTest.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
+#include "stdafx.h"
+#include "gtest-all-guard.hpp"
+#include <autowiring/autowiring.h>
+
+class ParallelTest:
+  public testing::Test
+{};
+
