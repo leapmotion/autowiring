fa6ec5f26bad5aadedf72bfe62fd6e28bd22b9e4
Jason Lokerson <cmercenary@gmail.com>
2015-07-31 14:58:49 -0700

    Fix Android build break
    Android doesn't have  `posix_memalign`, detect this case with preprocessor macros and compile accordingly.

--- src/autowiring/CreationRulesUnix.cpp
+++ src/autowiring/CreationRulesUnix.cpp
@@ -4,10 +4,14 @@
 #include <cstdlib>
 
 void* autowiring::aligned_malloc(size_t ncb, size_t align) {
+#if _POSIX_C_SOURCE < 200112L && _XOPEN_SOURCE < 600 && !defined(__APPLE__)
+  return memalign(align, ncb);
+#else
   void* pRetVal;
-  if(posix_memalign(&pRetVal, align, ncb))
+  if (posix_memalign(&pRetVal, align, ncb))
     return nullptr;
   return pRetVal;
+#endif
 }
 
 void autowiring::aligned_free(void* ptr) {
