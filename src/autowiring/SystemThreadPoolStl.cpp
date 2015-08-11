634d0400ebafe5ebbe47185fc77d03949ee0a004
James Donald <jdonald@leapmotion.com>
2015-08-11 11:19:57 -0700

    Add a comment to ensure care when this gets added back

--- src/autowiring/SystemThreadPoolStl.cpp
+++ src/autowiring/SystemThreadPoolStl.cpp
@@ -44,7 +44,8 @@ void SystemThreadPoolStl::OnStartUnsafe(void) {
     // Do nothing if the pool size was already set by someone else
     return;
 
-  // TODO:  Set this number according to std::thread::hardware_concurrency.  This can't
+  // TODO:  Set this number according to std::thread::hardware_concurrency (or
+  // get_nprocs() on gcc, to retain libstdc++ backwards-compatibility).  This can't
   // be done right now due to the fact that DispatchQueue has terrible concurrency
   // performance.
   while (m_outstanding < 2)
