dd960c7c50c49a2620b1836616140dc9bc865620
Jason Lokerson <cmercenary@gmail.com>
2015-08-13 15:39:26 -0700

    Add get-by-type support for autowiring tuple
    Necessary for `AutoPacket::Call` to work as expected

---
+++ autowiring/sum.h
@@ -0,0 +1,20 @@
+// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
+#pragma once
+
+namespace autowiring {
+  template<int... Ns>
+  struct sum;
+
+  template<int N, int... Ns>
+  struct sum<N, Ns...> :
+    sum<Ns...>
+  {
+    static const size_t value = N + sum<Ns...>::value;
+  };
+
+  template<>
+  struct sum<>
+  {
+    static const size_t value = 0;
+  };
+}
