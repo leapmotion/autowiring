2ace02343b8a0be7ff46d62e7270ffb960b1543a
Jason Lokerson <cmercenary@gmail.com>
2015-10-15 13:14:08 -0700

    Implement SetThreadPriority
    @gittyupagain implemented this functionality on a private branch awhile back, let's get it into mainline.

--- src/autowiring/CoreThreadLinux.cpp
+++ src/autowiring/CoreThreadLinux.cpp
@@ -4,6 +4,7 @@
 #include "BasicThreadStateBlock.h"
 #include <pthread.h>
 #include <sys/resource.h>
+#include <pthread.h>
 
 using std::chrono::seconds;
 using std::chrono::milliseconds;
@@ -22,4 +23,40 @@ void BasicThread::GetThreadTimes(std::chrono::milliseconds& kernelTime, std::chr
   getrusage(RUSAGE_SELF, &usage);
   kernelTime = std::chrono::duration_cast<milliseconds>(seconds(usage.ru_stime.tv_sec) + microseconds(usage.ru_stime.tv_usec));
   userTime = std::chrono::duration_cast<milliseconds>(seconds(usage.ru_utime.tv_sec) + microseconds(usage.ru_utime.tv_usec));
+}
+
+void BasicThread::SetThreadPriority(ThreadPriority threadPriority) {
+  struct sched_param param = { 0 };
+  int policy = SCHED_RR;
+  int percent = 0;
+
+  switch (threadPriority) {
+  case ThreadPriority::Idle:
+    policy = SCHED_IDLE;
+    percent = 0;
+    break;
+  case ThreadPriority::Lowest:
+    percent = 1;
+    break;
+  case ThreadPriority::BelowNormal:
+    percent = 20;
+    break;
+  case ThreadPriority::Normal:
+    percent = 50;
+    break;
+  case ThreadPriority::AboveNormal:
+    percent = 66;
+    break;
+  case ThreadPriority::Highest:
+    percent = 83;
+    break;
+  case ThreadPriority::TimeCritical:
+    percent = 100;
+    break;
+  default:
+    throw std::runtime_error("Attempted to assign an unrecognized thread priority");
+  }
+  param.sched_priority = PTHREAD_MIN_PRIORITY + (percent*(PTHREAD_MAX_PRIORITY - PTHREAD_MIN_PRIORITY) + 50) / 100;
+
+  pthread_setschedparam(m_thisThread.native_handle(), policy, &param);
 }
