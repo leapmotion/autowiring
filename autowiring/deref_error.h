6ead4cc9851c9af7ef3e7404bff3f7b74ef75295
Jason Lokerson <cmercenary@gmail.com>
2015-12-18 16:39:41 -0800

    Add extended information about null field dereferences
    Fixes #835, just adds a little bit more information about null dereference exceptions when they occur.

---
+++ autowiring/deref_error.h
@@ -0,0 +1,29 @@
+// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
+#pragma once
+#include "autowiring_error.h"
+
+template<class T>
+class AutowirableSlot;
+
+template<typename T>
+class AutowiredFast;
+
+namespace autowiring {
+  std::string GenerateExceptionTextAWF(const std::type_info& ti);
+  std::string GenerateExceptionTextAW(const std::type_info& ti);
+
+  class deref_error:
+    public autowiring_error
+  {
+  public:
+    template<typename T>
+    deref_error(const AutowirableSlot<T>&) :
+      autowiring_error(GenerateExceptionTextAW(typeid(T)))
+    {};
+
+    template<typename T>
+    deref_error(const AutowiredFast<T>&) :
+      autowiring_error(GenerateExceptionTextAWF(typeid(T)))
+    {};
+  };
+}
