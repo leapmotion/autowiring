5e2d50b32e34e2bc8623bbc55a37b90aed9f6026
Jason Lokerson <cmercenary@gmail.com>
2016-02-08 16:26:47 -0800

    Use case improvements for autowiring::parallel
    This type is a little hard to use because the underlying thread pool is unpredictable.  Typical use cases do not mind the construction expense of this type, and instead simply prefer that the behavior is predictable.  Therefore, optimize on these use cases, and manually maintain a thread pool for system use.

--- src/autowiring/Parallel.cpp
+++ src/autowiring/Parallel.cpp
@@ -10,9 +10,62 @@ void parallel_iterator<void>::operator++(int) {
 }
 
 parallel::parallel(void):
-  m_ctxt(AutoCurrentContext{})
+  parallel{ *CoreContext::CurrentContext(), std::thread::hardware_concurrency() }
 {}
 
-parallel::parallel(CoreContext& ctxt) :
-  m_ctxt(ctxt.shared_from_this())
+parallel::parallel(size_t concurrency) :
+  parallel{ *CoreContext::CurrentContext(), concurrency }
 {}
+
+parallel::parallel(CoreContext& ctxt, size_t concurrency):
+  m_ctxt(ctxt.shared_from_this())
+{
+  if (!concurrency)
+    concurrency = std::thread::hardware_concurrency();
+
+  auto block = m_block;
+
+  // Fire off a bunch of threads to do work:
+  while (concurrency--)
+    std::thread(
+      [block] {
+        while(block->owned)
+          try { block->dq.WaitForEvent(); }
+          catch(dispatch_aborted_exception&) {
+            // Expected behavior, things tearing down, end here
+            return;
+          }
+      }
+    ).detach();
+
+  // Configure our signal after everything else is done
+  onStopReg = ctxt.onShutdown += [this, block] {
+    std::lock_guard<std::mutex> lk(block->m_lock);
+    if (!block->owned)
+      // Status block already destroyed, we're in a teardown race
+      // Short-circuit here, no reason to double-call stop
+      return;
+
+    stop_unsafe();
+  };
+}
+
+parallel::~parallel(void) {
+  stop();
+}
+
+void parallel::stop(void) {
+  std::lock_guard<std::mutex> lk(m_block->m_lock);
+  stop_unsafe();
+}
+
+void parallel::stop_unsafe(void) {
+  // Trivial return check:
+  if (!m_block)
+    return;
+
+  m_block->owned = false;
+  m_ctxt->onShutdown -= onStopReg;
+  m_ctxt.reset();
+  m_block.reset();
+}
