6d0bd5919e406da2c800246880a4dd2f58aae879
Jason Lokerson <cmercenary@gmail.com>
2016-04-11 10:38:03 -0700

    Split autoboost stuff into its own library, modernize add_pch

--- cmake-modules/AddPCH.cmake
+++ cmake-modules/AddPCH.cmake
@@ -1,18 +1,12 @@
 #MSVC pch macro.  Copied from http://pastebin.com/84dm5rXZ and modified by Walter Gray
-macro(add_pch SourcesVar PrecompiledHeader PrecompiledSource)
+macro(add_pch TargetName PrecompiledHeader PrecompiledSource)
   if(MSVC)
-    set_source_files_properties(${PrecompiledSource}
-        PROPERTIES
-        COMPILE_FLAGS "/Yc${PrecompiledHeader}"
-        )
-    foreach( src_file ${${SourcesVar}} )
-        set_source_files_properties(
-            ${src_file}
-            PROPERTIES
-            COMPILE_FLAGS "/Yu${PrecompiledHeader}"
-            )
-    endforeach( src_file ${${SourcesVar}} )
-    list(APPEND ${SourcesVar} ${PrecompiledHeader} ${PrecompiledSource})
-    add_compile_options(/Yu)
+    set_source_files_properties(
+      ${PrecompiledSource}
+      PROPERTIES
+      COMPILE_FLAGS "/Yc${PrecompiledHeader}"
+    )
+    target_sources(${TargetName} PRIVATE ${PrecompiledHeader} ${PrecompiledSource})
+    target_compile_definitions(${TargetName} PRIVATE "/Yu${PrecompiledHeader}")
   endif(MSVC)
 endmacro(add_pch)
