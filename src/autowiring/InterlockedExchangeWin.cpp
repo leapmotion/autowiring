f50eb565d0d9b7547e6d294255441533d41498f9
Jason Lokerson <cmercenary@gmail.com>
2016-04-13 14:21:59 -0700

    Add specifiers where needed

--- src/autowiring/InterlockedExchangeWin.cpp
+++ src/autowiring/InterlockedExchangeWin.cpp
@@ -3,19 +3,19 @@
 #include "InterlockedExchange.h"
 #include <windows.h>
 
-void* exchange_acquire(void** dest, void* exchange) {
+void* autowiring::exchange_acquire(void** dest, void* exchange) {
   return InterlockedExchangePointer(dest, exchange);
 }
 
-void* exchange_release(void** dest, void* exchange) {
+void* autowiring::exchange_release(void** dest, void* exchange) {
   return InterlockedExchangePointer(dest, exchange);
 }
 
-long interlocked_add(long* dest, long addBy) {
+long autowiring::interlocked_add(long* dest, long addBy) {
   return InterlockedExchangeAdd(dest, addBy);
 }
 
-void* compare_exchange(void** dest, void* exchange, const void* comparand) {
+void* autowiring::compare_exchange(void** dest, void* exchange, const void* comparand) {
   return InterlockedCompareExchangePointer(
     dest,
     exchange,
