d1ec42307cb1f17b83620a87f963a005889728d5
Jason Lokerson <cmercenary@gmail.com>
2016-04-14 20:58:42 -0700

    Add copy constructor to optional
    We support move construction, might as well support copy construction as well.

---
+++ src/autowiring/test/OptionalTest.cpp
@@ -0,0 +1,37 @@
+// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
+#include "stdafx.h"
+#include <autowiring/optional.h>
+
+class OptionalTest :
+  public testing::Test
+{};
+
+TEST_F(OptionalTest, PrimitiveCheck) {
+  optional<int> x;
+  ASSERT_FALSE(x);
+  x = 10;
+  ASSERT_TRUE(x);
+  x = 0;
+  ASSERT_TRUE(x);
+  x = {};
+  ASSERT_FALSE(x);
+}
+
+TEST_F(OptionalTest, DestructionCheck) {
+  auto x = std::make_shared<int>(100);
+  {
+    optional<std::shared_ptr<int>> y = x;
+    ASSERT_EQ(2L, x.use_count());
+    y = {};
+    ASSERT_TRUE(x.unique());
+  }
+  ASSERT_TRUE(x.unique());
+}
+
+TEST_F(OptionalTest, MoveCheck) {
+  std::unique_ptr<int> u{ new int{55} };
+
+  optional<std::unique_ptr<int>> v = std::move(u);
+  ASSERT_EQ(nullptr, u);
+  ASSERT_EQ(55, **v);
+}
