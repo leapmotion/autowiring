ba6a4c527783a3961612d5f52b9b9236d771d168
Walter Gray <wgray@leapmotion.com>
2016-08-04 19:46:47 -0700

    Add femto second durations to chrono_types

--- src/autowiring/chrono_types.h
+++ src/autowiring/chrono_types.h
@@ -10,23 +10,29 @@ typedef std::chrono::duration<double> seconds_d;
 typedef std::chrono::duration<double, std::milli> milliseconds_d;
 typedef std::chrono::duration<double, std::micro> microseconds_d;
 typedef std::chrono::duration<double, std::nano> nanoseconds_d;
+typedef std::chrono::duration<double, std::femto> femtoseconds_d;
 
 typedef std::chrono::duration<float> seconds_f;
 typedef std::chrono::duration<float, std::milli> milliseconds_f;
 typedef std::chrono::duration<float, std::micro> microseconds_f;
 typedef std::chrono::duration<float, std::nano> nanoseconds_f;
+typedef std::chrono::duration<float, std::femto> femtoseconds_f;
 
 typedef std::chrono::duration<int32_t> seconds_i32;
 typedef std::chrono::duration<int32_t, std::milli> milliseconds_i32;
 typedef std::chrono::duration<int32_t, std::micro> microseconds_i32;
 typedef std::chrono::duration<int32_t, std::nano> nanoseconds_i32;
+typedef std::chrono::duration<int32_t, std::femto> femtoseconds_i32;
 
 typedef std::chrono::duration<int64_t> seconds_i64;
 typedef std::chrono::duration<int64_t, std::milli> milliseconds_i64;
 typedef std::chrono::duration<int64_t, std::micro> microseconds_i64;
 typedef std::chrono::duration<int64_t, std::nano> nanoseconds_i64;
+typedef std::chrono::duration<int64_t, std::femto> femtoseconds_i64;
 
 typedef basic_timer<std::chrono::profiling_clock> profiling_timer;
 typedef basic_timer<std::chrono::profiling_clock, seconds_d> profiling_timer_seconds;
 typedef basic_timer<std::chrono::profiling_clock, milliseconds_d> profiling_timer_milliseconds;
 typedef basic_timer<std::chrono::profiling_clock, microseconds_d> profiling_timer_microseconds;
+typedef basic_timer<std::chrono::profiling_clock, nanoseconds_d> profiling_timer_nanoseconds;
+typedef basic_timer<std::chrono::profiling_clock, femtoseconds_d> profiling_timer_femtoseconds;
