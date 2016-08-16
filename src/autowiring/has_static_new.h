1fcd194bf8af281c6d925016750e1f9b8dd65797
Jason Lokerson <cmercenary@gmail.com>
2016-08-16 15:22:43 -0700

    Need an rvalue cast here
    Otherwise this will be treated as a reference

--- src/autowiring/has_static_new.h
+++ src/autowiring/has_static_new.h
@@ -32,7 +32,7 @@ template<typename T, typename... Args>
 struct has_static_new
 {
   template<class U>
-  static std::true_type select(decltype(U::New(*(Args*)nullptr...))*);
+  static std::true_type select(decltype(U::New((Args&&)*(Args*)nullptr...))*);
 
   template<class U>
   static std::false_type select(...);
