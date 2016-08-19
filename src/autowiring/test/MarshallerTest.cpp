d970c8c73da5678e0d732a408dac80f782b609a3
Walter Gray <wgray@leapmotion.com>
2016-08-19 14:35:27 -0700

    Temporarily disable float marshalling until a suitable implementation is selected.

--- src/autowiring/test/MarshallerTest.cpp
+++ src/autowiring/test/MarshallerTest.cpp
@@ -6,14 +6,37 @@ class MarshallerTest:
   public testing::Test
 {};
 
-TEST_F(MarshallerTest, DoubleMarshalTest) {
+//TEMPORARILY DISABLED - Floating point marshaling is a hard problem and should
+//be handled by a library. See Dragon4 and Grisu3, or https://github.com/google/double-conversion.
+
+/*TEST_F(MarshallerTest, FloatTest) {
+  autowiring::marshaller<float> b;
+
+  float x = 0;
+  std::string valX = b.marshal(&x);
+  ASSERT_STREQ("0", valX.c_str());
+
+  float outX = 20;
+  b.unmarshal(&outX, valX.c_str());
+  ASSERT_EQ(outX, x);
+
+  float y = 0.0001f;
+  std::string valY = b.marshal(&y);
+  ASSERT_STREQ("0.0001", valY.c_str());
+
+  float z = 1.23e10f;
+  std::string valZ = b.marshal(&z);
+  ASSERT_STREQ("12300000000", valZ.c_str());
+}
+
+TEST_F(MarshallerTest, DoubleTest) {
   autowiring::marshaller<double> b;
 
   double x = -0.00899999999999999;
   std::string val = b.marshal(&x);
   ASSERT_STREQ("-0.00899999999999999", val.c_str());
 }
-
+*/
 TEST_F(MarshallerTest, NegativeValues) {
   autowiring::marshaller<int> b;
 
@@ -36,16 +59,16 @@ TEST_F(MarshallerTest, UnsignedValues) {
   int x = 0;
   std::string valX = b.marshal(&x);
   ASSERT_STREQ("0", valX.c_str());
-}
 
-TEST_F(MarshallerTest, RoundTripTest) {
-  autowiring::marshaller<unsigned int> b;
+  unsigned int y = std::numeric_limits<uint32_t>::max();
+  std::string valY = b.marshal(&y);
+  ASSERT_STREQ("4294967295", valY.c_str());
 
-  unsigned int x = 20;
-  std::string valX = b.marshal(&x);
+  unsigned int z = 20;
+  std::string valZ = b.marshal(&z);
 
-  unsigned int outX = 0;
-  b.unmarshal(&outX, valX.c_str());
+  unsigned int outZ = 0;
+  b.unmarshal(&outZ, valZ.c_str());
 
-  ASSERT_EQ(outX, x);
+  ASSERT_EQ(outZ, z);
 }
