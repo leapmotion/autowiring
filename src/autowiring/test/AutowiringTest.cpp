c13c084d04b175cc13592e2bb374649689de6c6d
Jason Lokerson <cmercenary@gmail.com>
2016-09-19 06:08:22 -0700

    Add the version number pieces and a formatting double-check

--- src/autowiring/test/AutowiringTest.cpp
+++ src/autowiring/test/AutowiringTest.cpp
@@ -7,6 +7,14 @@
 #include <autowiring/AutowiringDebug.h>
 #include <autowiring/CoreThread.h>
 
+// Version number should have at least two dots, three digits, and a null terminator
+static_assert(sizeof(AUTOWIRING_VERSION) >= 2 + 3 + 1, "Version number string appears to be incorrect");
+
+// Other version number fields should be at least one character each
+static_assert(sizeof(AUTOWIRING_VERSION_MAJOR) >= 2, "Major version number component appears to be incorrect");
+static_assert(sizeof(AUTOWIRING_VERSION_MINOR) >= 2, "Minor version number component appears to be incorrect");
+static_assert(sizeof(AUTOWIRING_VERSION_PATCH) >= 2, "Patch version number component appears to be incorrect");
+
 int main(int argc, const char* argv []) {
   autowiring::dbg::DebugInit();
   return autotesting_main(argc, argv);
