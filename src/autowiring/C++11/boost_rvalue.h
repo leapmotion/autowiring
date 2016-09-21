8467e85e8881cc12a22c11bd336f255df41572cb
James Donald <jdonald@leapmotion.com>
2016-09-21 12:55:40 -0700

    Remove specific now-unused boost headers

--- src/autowiring/C++11/boost_rvalue.h
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
-#pragma once
-
-#include <autoboost/move/move.hpp>
-
-namespace std {
-  using autoboost::forward;
-  using autoboost::move;
-}
--- src/autowiring/C++11/boost_thread.h
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
-#pragma once
-
-#include <autoboost/thread/thread.hpp>
-
-namespace std {
-  using autoboost::thread;
-  namespace this_thread = autoboost::this_thread;
-}
