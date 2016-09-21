c024597d4d9b75b1f542ed0ffbd45fa6dda5c337
James Donald <jdonald@leapmotion.com>
2016-09-21 12:46:25 -0700

    Remove unneeded TR1 headers

--- src/autowiring/C++11/tr1_unordered_map.h
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2012-2015 Leap Motion, Inc. All rights reserved.
-#pragma once
-
-#include <tr1/unordered_map>
-
-namespace std {
-  using std::tr1::unordered_map;
-  using std::tr1::hash;
-}
