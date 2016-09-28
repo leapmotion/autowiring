f5a03a7121e49fb2a615ad89a76f93f70aef960f
Walter Gray <wgray@leapmotion.com>
2016-09-28 15:43:06 -0700

    Add better Floating point tests

--- src/autowiring/test/MarshallerTest.cpp
+++ src/autowiring/test/MarshallerTest.cpp
@@ -6,12 +6,33 @@ class MarshallerTest:
   public testing::Test
 {};
 
+TEST_F(MarshallerTest, FloatTest) {
+  autowiring::marshaller<float> f;
+
+  float x = 0;
+  std::string valX = f.marshal(&x);
+  ASSERT_STREQ("0", valX.c_str()) << "Failed to properly output 0";
+
+  float outX = 20;
+  f.unmarshal(&outX, valX.c_str());
+  ASSERT_EQ(outX, 0) << "Failed to properly round trip 0";
+
+  float y = 0.0001f;
+  std::string valY = f.marshal(&y);
+  ASSERT_STREQ("0.0001", valY.c_str()) << "Failed to marshal a decimal value";
+
+  float z = 1.23e8f;
+  std::string valZ = f.marshal(&z);
+  ASSERT_STREQ("123000000", valZ.c_str()) << "Failed to marshal a value with an exponent";
+}
+}
+
 TEST_F(MarshallerTest, DoubleMarshalTest) {
-  autowiring::marshaller<double> b;
+  autowiring::marshaller<double> d;
 
   double x = -0.00899999999999999;
-  std::string val = b.marshal(&x);
-  ASSERT_STREQ("-0.00899999999999999", val.c_str());
+  std::string val = d.marshal(&x);
+  ASSERT_STREQ("-0.00899999999999999", val.c_str()) << "Failed to marshal a double-precision value";
 }
 
 TEST_F(MarshallerTest, NegativeValues) {
