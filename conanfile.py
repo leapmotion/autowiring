2eb836df21f7ea8c86ced8ddc478f97f43627eaf
Walter Gray <wgray@leapmotion.com>
2016-11-30 13:48:16 -0800

    1st working build

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,38 @@
-from conans import ConanFile, CMake
-import re
-import os
+from conans import ConanFile, CMake, tools
+import os, re, sys
 
 class AutowiringConan(ConanFile):
-    name = "Autowiring"
+    name = "autowiring"
+    version = "1.0.4"
+    #re.match(".*VERSION\s+([0-9|\.]*)\)", open("%s/version.cmake" % sys.argv[0]).read()).group(1)
+    license = "Apache-2.0"
+    author = "Leap Motion Inc. (www.leapmotion.com)"
+    url = "https://github.com/leapmotion/autowiring.git"
     settings = "os", "compiler", "build_type", "arch"
-    exports = "*"
-    version = re.match(".*VERSION\s+([0-9|\.]*)\)",open("version.cmake").read()).group(1)
+    exports = "cmake-modules/*", "contrib/*", "devguide/*", "examples/*", "nuget/*", "scripts/*", "src/*", "standard/*", \
+                ".travis.yml", "Autowiring.nuspec.in", "CMakeLists.txt", "CONTRIBUTORS.txt", "Doxyfile", "LICENSE.txt", "README.md", "publicDoxyfile.conf", "tests.cmake", "version.cmake"
+    generators = "cmake"
+    build_dir = "b"
+    short_paths = True
 
     def build(self):
         self.output.info(self.version)
-        build_dir="b"
-        if not os.path.exists(build_dir):
-            os.makedirs(build_dir)
+        
+        if not os.path.exists(self.build_dir):
+            os.makedirs(self.build_dir)
         
         cmake = CMake(self.settings)
         args = ['-DCMAKE_INSTALL_PREFIX="%s"' % self.package_folder]
 
-        self.run('cd %s && cmake %s %s %s' % (build_dir, self.conanfile_directory, cmake.command_line, ' '.join(args)))
-        self.run("cd %s && cmake --build . %s" % (build_dir, cmake.build_config))
+        if self.settings.compiler == "Visual Studio" and self.settings.build_type == "Debug":
+            if not str(self.settings.compiler.runtime).endswith("d"):
+                self.settings.compiler.runtime = str(self.settings.compiler.runtime) + "d"
+
+        self.run('cd %s && cmake %s %s %s' % (self.build_dir, self.conanfile_directory, cmake.command_line, ' '.join(args)))
+        self.run("cd %s && cmake --build . %s --target install" % (self.build_dir, cmake.build_config))
 
     def package(self):
-        self.output.info("handled by the install step")
+        self.output.info(self.package_folder)
 
     def package_info(self):
         self.cpp_info.libs = ["hello"]
