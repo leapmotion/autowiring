15a740c5b9260ad36dbec4674e9558487a75108c
James Donald <jdonald@leapmotion.com>
2017-11-15 12:25:06 -0800

    Revert "Merge pull request #1025 from leapmotion/upd-json11"
    This reverts commit 4da8d6a405410a7d857b1529d88ebfbc6fe469eb, reversing
    changes made to dccea5023406402a4804e947982a4677373b5368.

--- contrib/json11/json11.hpp
+++ contrib/json11/json11.hpp
@@ -54,25 +54,19 @@
 #include <vector>
 #include <map>
 #include <memory>
-#include <initializer_list>
 
 #ifdef _MSC_VER
-    #if _MSC_VER <= 1800 // VS 2013
-        #ifndef noexcept
-            #define noexcept throw()
-        #endif
-
-        #ifndef snprintf
-            #define snprintf _snprintf_s
-        #endif
-    #endif
+  // noexcept was added to MSVC in Visual Studio 2014
+  #if _MSC_VER > 1800
+    #define JSON11_NOEXCEPT noexcept
+  #else
+    #define JSON11_NOEXCEPT throw()
+  #endif
+#else
+  #define JSON11_NOEXCEPT noexcept
 #endif
 
 namespace json11 {
-
-enum JsonParse {
-    STANDARD, COMMENTS
-};
 
 class JsonValue;
 
@@ -88,11 +82,11 @@ public:
     typedef std::map<std::string, Json> object;
 
     // Constructors for the various types of JSON value.
-    Json() noexcept;                // NUL
-    Json(std::nullptr_t) noexcept;  // NUL
+    Json() JSON11_NOEXCEPT;         // NUL
+    Json(std::nullptr_t);           // NUL
     Json(double value);             // NUMBER
     Json(int value);                // NUMBER
-    Json(bool value);               // BOOL
+    Json(bool value) JSON11_NOEXCEPT; // BOOL
     Json(const std::string &value); // STRING
     Json(std::string &&value);      // STRING
     Json(const char * value);       // STRING
@@ -106,17 +100,32 @@ public:
     Json(const T & t) : Json(t.to_json()) {}
 
     // Implicit constructor: map-like objects (std::map, std::unordered_map, etc)
-    template <class M, typename std::enable_if<
-        std::is_constructible<std::string, decltype(std::declval<M>().begin()->first)>::value
-        && std::is_constructible<Json, decltype(std::declval<M>().begin()->second)>::value,
-            int>::type = 0>
-    Json(const M & m) : Json(object(m.begin(), m.end())) {}
+    template <class M>
+    Json(
+        const typename std::enable_if<
+            std::is_constructible<std::string, decltype(std::declval<M>().begin()->first)>::value &&
+            std::is_constructible<std::string, decltype(std::declval<M>().begin()->second)>::value,
+            M
+        >::type& m
+    ) :
+        Json(object(m.begin(), m.end()))
+    {}
 
     // Implicit constructor: vector-like objects (std::list, std::vector, std::set, etc)
-    template <class V, typename std::enable_if<
-        std::is_constructible<Json, decltype(*std::declval<V>().begin())>::value,
-            int>::type = 0>
-    Json(const V & v) : Json(array(v.begin(), v.end())) {}
+    template <class V>
+    Json(
+        const typename std::enable_if<
+            std::is_constructible<Json, decltype(std::declval<V>().begin()->first)>::value,
+            V
+        >::type& v
+    ) :
+        Json(array(v.begin(), v.end()))
+    {}
+
+    template<class V>
+    Json(V* begin, V* end):
+      Json(array(begin, end))
+    {}
 
     // This prevents Json(some_pointer) from accidentally producing a bool. Use
     // Json(bool(some_pointer)) if that behavior is desired.
@@ -161,33 +170,17 @@ public:
     }
 
     // Parse. If parse fails, return Json() and assign an error message to err.
-    static Json parse(const std::string & in,
-                      std::string & err,
-                      JsonParse strategy = JsonParse::STANDARD);
-    static Json parse(const char * in,
-                      std::string & err,
-                      JsonParse strategy = JsonParse::STANDARD) {
+    static Json parse(const std::string & in, std::string & err);
+    static Json parse(const char * in, std::string & err) {
         if (in) {
-            return parse(std::string(in), err, strategy);
+            return parse(std::string(in), err);
         } else {
             err = "null input";
             return nullptr;
         }
     }
     // Parse multiple objects, concatenated or separated by whitespace
-    static std::vector<Json> parse_multi(
-        const std::string & in,
-        std::string::size_type & parser_stop_pos,
-        std::string & err,
-        JsonParse strategy = JsonParse::STANDARD);
-
-    static inline std::vector<Json> parse_multi(
-        const std::string & in,
-        std::string & err,
-        JsonParse strategy = JsonParse::STANDARD) {
-        std::string::size_type parser_stop_pos;
-        return parse_multi(in, parser_stop_pos, err, strategy);
-    }
+    static std::vector<Json> parse_multi(const std::string & in, std::string & err);
 
     bool operator== (const Json &rhs) const;
     bool operator<  (const Json &rhs) const;
@@ -195,14 +188,6 @@ public:
     bool operator<= (const Json &rhs) const { return !(rhs < *this); }
     bool operator>  (const Json &rhs) const { return  (rhs < *this); }
     bool operator>= (const Json &rhs) const { return !(*this < rhs); }
-
-    /* has_shape(types, err)
-     *
-     * Return true if this is a JSON object and, for each item in types, has a field of
-     * the given type. If not, return false and set err to a descriptive message.
-     */
-    typedef std::initializer_list<std::pair<std::string, Type>> shape;
-    bool has_shape(const shape & types, std::string & err) const;
 
 private:
     std::shared_ptr<JsonValue> m_ptr;
