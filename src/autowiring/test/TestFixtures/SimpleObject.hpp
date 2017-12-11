8d81fffb52f67bf5ecb331a131416c0d9c78f445
James Donald <jdonald@leapmotion.com>
2017-12-11 13:28:15 -0800

    Restore SimpleObject.hpp for AutowiringTest.cpp

---
+++ src/autowiring/test/TestFixtures/SimpleObject.hpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2012-2017 Leap Motion, Inc. All rights reserved.
+#pragma once
+#include <autowiring/ContextMember.h>
+#include <autowiring/TypeUnifier.h>
+#include TYPE_TRAITS_HEADER
+
+// Simple ContextMember class:
+class SimpleObject:
+  public ContextMember
+{
+public:
+  SimpleObject(void):
+    zero(0),
+    one(1)
+  {}
+
+  ~SimpleObject(void) {
+  }
+
+  int zero;
+  int one;
+};
+
+static_assert(
+  std::is_same<
+    SimpleObject,
+    typename autowiring::SelectTypeUnifier<SimpleObject>::type
+  >::value,
+  "The SimpleObject was incorrectly identified as needing a type unifier"
+);
