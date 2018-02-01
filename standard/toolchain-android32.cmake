16bf7d586f8e37caf767169bec695d4c14249601
Walter Gray <wgray@leapmotion.com>
2018-02-01 15:28:11 -0800

    default to armeabi on android32

--- standard/toolchain-android32.cmake
+++ standard/toolchain-android32.cmake
@@ -1,2 +1,4 @@
-set(CMAKE_ANDROID_ARCH_ABI armeabi-v7a)
+if(NOT DEFINED CMAKE_ANDROID_ARCH_ABI)
+  set(CMAKE_ANDROID_ARCH_ABI armeabi)
+endif()
 include(${CMAKE_CURRENT_LIST_DIR}/toolchain-android.cmake)
