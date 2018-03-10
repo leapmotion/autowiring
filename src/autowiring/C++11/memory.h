a4e01229d217e180dd8c94dbddc814d60274c6ee
Jonathan Marsden <jmarsden@leapmotion.com>
2018-03-09 16:25:09 -0800

    Only use alternate make_unique if pre-C++14

--- src/autowiring/C++11/memory.h
+++ src/autowiring/C++11/memory.h
@@ -3,7 +3,7 @@
 
 #include <memory>
 
-// MSVC already implements make_unique
-#ifndef _MSC_VER
+// MSVC and C++14 already implements make_unique
+#if !defined(_MSC_VER) && (__cplusplus < 201402L)
   #include "make_unique.h"
 #endif
